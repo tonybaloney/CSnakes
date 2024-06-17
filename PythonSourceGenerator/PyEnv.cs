ad0bce87410241123888240378227f2e363199cd
Anthony Shaw <anthony.p.shaw@gmail.com>
2024-06-17 15:55:47 +1000

    additional changes

---
+++ PythonSourceGenerator/PyEnv.cs
@@ -0,0 +1,63 @@
+﻿using Python.Runtime;
+using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace PythonSourceGenerator
+{
+    internal class PyEnv
+    {
+        private readonly List<string> paths = new List<string>();
+
+        private static string MapVersion(string version)
+        {
+            return version.Replace(".", "");
+        }
+
+        public PyEnv(string home, string version = "3.10")
+        {
+            string versionPath = MapVersion(version);
+            if (PythonEngine.IsInitialized)
+            {
+                // Raise exception?
+                return;
+            }
+
+#pragma warning disable RS1035
+            string dllPath = Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData);
+#pragma warning restore RS1035
+            dllPath = Path.Combine(dllPath, "Programs", "Python", "Python" + versionPath);
+            Runtime.PythonDLL = Path.Combine(dllPath, string.Format("python{0}.dll", versionPath));
+
+            if (!string.IsNullOrEmpty(home))
+            {
+                PythonEngine.PythonHome = home;
+                // TODO : Path sep is : on Unix
+                PythonEngine.PythonPath = Path.Combine(dllPath, "Lib") + ";" + home;
+                paths.Add(Path.Combine(dllPath, "Lib"));
+                paths.Add(home);
+            }
+            else
+            {
+                PythonEngine.PythonPath = Path.Combine(dllPath, "Lib");
+                paths.Add(Path.Combine(dllPath, "Lib"));
+            }
+
+            // TODO : Add virtual env paths
+            PythonEngine.Initialize();
+        }
+
+        public PyObject LoadModule(string module)
+        {
+            return Py.Import(module);
+        }
+
+        public void AddPath(string path)
+        {
+            if (paths.Contains(path))
+                return;
+            paths.Add(path);
+            PythonEngine.PythonPath = string.Join(";", paths);
+        }
+    }
+}

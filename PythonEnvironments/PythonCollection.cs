b82e76ee8329c94c2ad7904ae3eae2373a2bb1f0
Aaron Powell <me@aaron-powell.com>
2024-07-02 12:00:59 +1000

    Cleanup for how list is done. Should be just an enumerable

---
+++ PythonEnvironments/PythonCollection.cs
@@ -0,0 +1,11 @@
+﻿using Python.Runtime;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace PythonEnvironments;
+
+public static class PythonCollection
+{
+    public static IEnumerable<T> AsEnumerable<T>(this PyObject obj) =>
+        new PyIterable(obj.GetIterator()).Select(item => item.As<T>());
+}

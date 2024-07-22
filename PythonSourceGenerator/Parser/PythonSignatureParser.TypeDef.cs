b4d28aa890208991cb9ff04d94643f9c736e26a2
Aaron Powell <me@aaron-powell.com>
2024-07-22 13:22:03 +1000

    Null typing

--- PythonSourceGenerator/Parser/PythonSignatureParser.TypeDef.cs
+++ PythonSourceGenerator/Parser/PythonSignatureParser.TypeDef.cs
@@ -5,13 +5,12 @@ using Superpower.Parsers;
 namespace PythonSourceGenerator.Parser;
 public static partial class PythonSignatureParser
 {
-    public static TokenListParser<PythonSignatureTokens.PythonSignatureToken, PythonTypeSpec> PythonTypeDefinitionTokenizer { get; } =
+    public static TokenListParser<PythonSignatureTokens.PythonSignatureToken, PythonTypeSpec?> PythonTypeDefinitionTokenizer { get; } =
         (from name in Token.EqualTo(PythonSignatureTokens.PythonSignatureToken.Identifier).Or(Token.EqualTo(PythonSignatureTokens.PythonSignatureToken.None))
          from openBracket in Token.EqualTo(PythonSignatureTokens.PythonSignatureToken.OpenBracket)
             .Then(_ => PythonTypeDefinitionTokenizer.ManyDelimitedBy(
                 Token.EqualTo(PythonSignatureTokens.PythonSignatureToken.Comma),
-                end: Token.EqualTo(PythonSignatureTokens.PythonSignatureToken.CloseBracket)
-                ))
+                Token.EqualTo(PythonSignatureTokens.PythonSignatureToken.CloseBracket)))
             .OptionalOrDefault()
          select new PythonTypeSpec(name.ToStringValue(), openBracket))
         .Named("Type Definition");

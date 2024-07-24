a41bae889a737b7ac0e28c134dd0205532437ab4
Anthony Shaw <anthony.p.shaw@gmail.com>
2024-07-24 14:32:56 +1000

    Simplify the parsers by using as much of the .NET standard as possible. Support underscores in all literal types

--- PythonSourceGenerator/Parser/PythonSignatureParser.Constants.cs
+++ PythonSourceGenerator/Parser/PythonSignatureParser.Constants.cs
@@ -2,6 +2,7 @@
 using Superpower;
 using Superpower.Model;
 using Superpower.Parsers;
+using System;
 using System.Globalization;
 
 namespace PythonSourceGenerator.Parser;
@@ -19,17 +20,17 @@ public static partial class PythonSignatureParser
     public static TextParser<Unit> DecimalConstantToken { get; } =
             from sign in Character.EqualTo('-').OptionalOrDefault()
             from first in Character.Digit
-            from rest in Character.Digit.Or(Character.In('.', 'e', 'E', '+', '-')).IgnoreMany()
+            from rest in UnderScoreOrDigit.Or(Character.In('.', 'e', 'E', '+', '-')).IgnoreMany()
             select Unit.Value;
 
     public static TextParser<Unit> HexidecimalConstantToken { get; } =
         from prefix in Span.EqualTo("0x")
-        from digits in Character.HexDigit.AtLeastOnce()
+        from digits in Character.EqualTo('_').Or(Character.HexDigit).AtLeastOnce()
         select Unit.Value;
 
     public static TextParser<Unit> BinaryConstantToken { get; } =
         from prefix in Span.EqualTo("0b")
-        from digits in Character.EqualTo('0').Or(Character.EqualTo('1')).AtLeastOnce()
+        from digits in Character.In('0', '1', '_').AtLeastOnce()
         select Unit.Value;
 
     public static TextParser<Unit> DoubleQuotedStringConstantToken { get; } =
@@ -58,31 +59,28 @@ public static partial class PythonSignatureParser
 
     public static TokenListParser<PythonSignatureTokens.PythonSignatureToken, PythonConstant> DecimalConstantTokenizer { get; } =
         Token.EqualTo(PythonSignatureTokens.PythonSignatureToken.Decimal)
-        .Apply(ConstantParsers.DecimalDoubleWithExponents)
-        .Select(d => new PythonConstant(d))
+        .Select(token => new PythonConstant(double.Parse(token.ToStringValue().Replace("_", ""), NumberStyles.Float, CultureInfo.InvariantCulture)))
         .Named("Decimal Constant");
 
     public static TokenListParser<PythonSignatureTokens.PythonSignatureToken, PythonConstant> IntegerConstantTokenizer { get; } =
         Token.EqualTo(PythonSignatureTokens.PythonSignatureToken.Integer)
-        .Apply(ConstantParsers.IntegerInt64)
-        .Select(d => new PythonConstant(d))
+        .Select(d => new PythonConstant(long.Parse(d.ToStringValue().Replace("_", ""), NumberStyles.Integer)))
         .Named("Integer Constant");
 
     public static TokenListParser<PythonSignatureTokens.PythonSignatureToken, PythonConstant> HexidecimalIntegerConstantTokenizer { get; } =
         Token.EqualTo(PythonSignatureTokens.PythonSignatureToken.HexidecimalInteger)
-        .Apply(ConstantParsers.HexidecimalConstantParser)
-        .Select(d => new PythonConstant { Type = PythonConstant.ConstantType.HexidecimalInteger, IntegerValue = (long)d })
+        .Select(d => new PythonConstant { Type = PythonConstant.ConstantType.HexidecimalInteger, IntegerValue = long.Parse(d.ToStringValue().Substring(2).Replace("_", ""), NumberStyles.HexNumber) })
         .Named("Hexidecimal Integer Constant");
-    
+
     public static TokenListParser<PythonSignatureTokens.PythonSignatureToken, PythonConstant> BinaryIntegerConstantTokenizer { get; } =
         Token.EqualTo(PythonSignatureTokens.PythonSignatureToken.BinaryInteger)
-        .Apply(ConstantParsers.BinaryConstantParser)
-        .Select(d => new PythonConstant { Type = PythonConstant.ConstantType.BinaryInteger, IntegerValue = (long)d })
+        // TODO: Consider Binary Format specifier introduced in .NET 8 https://learn.microsoft.com/en-us/dotnet/standard/base-types/standard-numeric-format-strings#binary-format-specifier-b
+        .Select(d => new PythonConstant { Type = PythonConstant.ConstantType.BinaryInteger, IntegerValue = (long)Convert.ToUInt64(d.ToStringValue().Substring(2).Replace("_", ""), 2) })
         .Named("Binary Integer Constant");
 
     public static TokenListParser<PythonSignatureTokens.PythonSignatureToken, PythonConstant> BoolConstantTokenizer { get; } =
         Token.EqualTo(PythonSignatureTokens.PythonSignatureToken.True).Or(Token.EqualTo(PythonSignatureTokens.PythonSignatureToken.False))
-        .Select(d => new PythonConstant(d.Kind == PythonSignatureTokens.PythonSignatureToken.True ))
+        .Select(d => new PythonConstant(d.Kind == PythonSignatureTokens.PythonSignatureToken.True))
         .Named("Bool Constant");
 
     public static TokenListParser<PythonSignatureTokens.PythonSignatureToken, PythonConstant> NoneConstantTokenizer { get; } =
@@ -127,59 +125,5 @@ public static partial class PythonSignatureParser
                 .Many()
             from close in Character.EqualTo('\'')
             select new string(chars);
-
-        public static TextParser<UInt64> HexidecimalConstantParser { get; } =
-            from prefix in Span.EqualTo("0x")
-            from digits in Character.HexDigit.AtLeastOnce()
-            select UInt64.Parse(new string(digits), System.Globalization.NumberStyles.HexNumber);
-
-        public static TextParser<UInt64> BinaryConstantParser { get; } =
-            from prefix in Span.EqualTo("0b")
-            // TODO: Consider Binary Format specifier introduced in .NET 8 https://learn.microsoft.com/en-us/dotnet/standard/base-types/standard-numeric-format-strings#binary-format-specifier-b
-            from digits in Character.EqualTo('0').Or(Character.EqualTo('1')).AtLeastOnce()
-            select Convert.ToUInt64(new string(digits), 2);
-
-        // Based on Numerics.IntegerInt64 but with Python characteristics (underscores after the first digit)
-        public static TextParser<long> IntegerInt64 { get; } = delegate (TextSpan input)
-        {
-            bool negative = false;
-            Result<char> result = input.ConsumeChar();
-            if (!result.HasValue)
-            {
-                return Result.Empty<long>(input, ["sign", "digit"]);
-            }
-            if (result.Value == '-')
-            {
-                negative = true;
-                result = result.Remainder.ConsumeChar();
-            }
-            else if (result.Value == '+')
-            {
-                result = result.Remainder.ConsumeChar();
-            }
-            if (!result.HasValue || !char.IsDigit(result.Value))
-            {
-                return Result.Empty<long>(input, ["digit"]);
-            }
-            long num = 0L;
-
-            TextSpan remainder ;
-            do
-            {
-                if (result.Value != '_')
-                {
-                    num = 10 * num + (result.Value - 48);
-                }
-                remainder = result.Remainder;
-                result = remainder.ConsumeChar();
-            }
-            while (result.HasValue && (char.IsDigit(result.Value) || result.Value == '_'));
-
-            if (negative) num = -num;
-
-            return Result.Value(num, input, remainder);
-        };
-
-        public static TextParser<double> DecimalDoubleWithExponents { get; } = Numerics.Decimal.Select((TextSpan span) => double.Parse(span.ToStringValue(), NumberStyles.Float, CultureInfo.InvariantCulture));
     }
 }

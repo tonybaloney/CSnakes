4611acfc2bbd0baeec21ccfbdde619261fc61299
Aaron Powell <me@aaron-powell.com>
2024-08-01 15:02:43 +1000

    Samples project now runs from nuget packages consistently

--- samples/QuickConsoleTest/Program.cs
+++ samples/QuickConsoleTest/Program.cs
@@ -1,43 +1,59 @@
-﻿using Python.Generated;
-using PythonEnvironments;
+﻿using CSnakes.Runtime;
 using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Text.Json;
 
-// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+var builder = PythonEnvironment.FromNuGet("3.12.4") ?? throw new Exception("Cannot find Python");
 
-var builder = (PythonEnvironment.FromNuget("3.12.4")
-    ?.WithVirtualEnvironment(Path.Join(Environment.CurrentDirectory, "..", "..", "..", "..", "ExamplePythonDependency", ".venv"))) ?? throw new Exception("Cannot find Python");
+builder.WithVirtualEnvironment(Path.Join(Environment.CurrentDirectory, "..", "..", "..", "..", "ExamplePythonDependency", ".venv"));
 
 using var env = builder.Build(Path.Join(Environment.CurrentDirectory, "..", "..", "..", "..", "ExamplePythonDependency"));
 
-var quickDemo = env.QuickDemo();
-Console.WriteLine(quickDemo.Scream("a", 99));
-Console.WriteLine(string.Join(',', quickDemo.ScreamNames(["a", "b", "c"], 3)));
+RunQuickDemo(env);
 
-var dict = env.TypeDemos().ReturnDict();
+RunDictionaryDemo(env);
 
-foreach ((string key, long value) in dict)
+RunKmeansDemo(env);
+
+RunAIDemo(env);
+
+static void RunQuickDemo(IPythonEnvironment env)
 {
-    Console.WriteLine($"{key} : {value}");
+    var quickDemo = env.QuickDemo();
+    Console.WriteLine(quickDemo.Scream("a", 99));
+    Console.WriteLine(string.Join(',', quickDemo.ScreamNames(["a", "b", "c"], 3)));
 }
 
-env.TypeDemos().TakeDict(dict);
-// That was boring, how about scikit-learn
-var kmeansExample = env.KmeansExample();
-List<(long, long)> data = [
-    (1, 2), (1, 4), (1, 0),
+static void RunDictionaryDemo(IPythonEnvironment env)
+{
+    var dict = env.TypeDemos().ReturnDict();
+    foreach ((string key, long value) in dict)
+    {
+        Console.WriteLine($"{key} : {value}");
+    }
+
+    env.TypeDemos().TakeDict(dict);
+}
+
+static void RunKmeansDemo(IPythonEnvironment env)
+{
+    // That was boring, how about scikit-learn
+    var kmeansExample = env.KmeansExample();
+    List<(long, long)> data = [
+        (1, 2), (1, 4), (1, 0),
         (10, 2), (10, 4), (10, 0)
-];
+    ];
 
-var inertiaResult = kmeansExample.CalculateKmeansInertia(data, 4);
-var centroids = JsonSerializer.Serialize(inertiaResult.Item1);
-var inertia = inertiaResult.Item2;
-Console.WriteLine($"KMeans inertia for 4 clusters is {centroids}, inertia is {inertia}");
+    var inertiaResult = kmeansExample.CalculateKmeansInertia(data, 4);
+    var centroids = JsonSerializer.Serialize(inertiaResult.Item1);
+    var inertia = inertiaResult.Item2;
+    Console.WriteLine($"KMeans inertia for 4 clusters is {centroids}, inertia is {inertia}");
+}
 
-
-var phi3demo = env.Phi3Demo();
-// var result = phi3demo.Phi3InferenceDemo("What kind of food is Brie?");
-// Console.WriteLine(result);
+static void RunAIDemo(IPythonEnvironment env)
+{
+    var phi3demo = env.Phi3Demo();
+    // var result = phi3demo.Phi3InferenceDemo("What kind of food is Brie?");
+    // Console.WriteLine(result);
+}

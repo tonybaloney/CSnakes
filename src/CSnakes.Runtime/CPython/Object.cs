ce0a2420b757521dd156daf54ae0dee26dbc9f71
Anthony Shaw <anthony.p.shaw@gmail.com>
2024-08-06 11:30:39 +1000

    Start layout for CPython API

---
+++ src/CSnakes.Runtime/CPython/Object.cs
@@ -0,0 +1,27 @@
+﻿using Python.Runtime;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSnakes.Runtime.CPython;
+internal unsafe partial class CPythonAPI
+{
+    [LibraryImport("python.dll")]
+    static partial PyObject* PyObject_Repr(PyObject* ob);
+
+
+/*    PyAPI_FUNC(PyObject*) PyObject_Str(PyObject*);
+    PyAPI_FUNC(PyObject*) PyObject_ASCII(PyObject*);
+    PyAPI_FUNC(PyObject*) PyObject_Bytes(PyObject*);
+    PyAPI_FUNC(PyObject*) PyObject_RichCompare(PyObject*, PyObject*, int);
+    PyAPI_FUNC(int) PyObject_RichCompareBool(PyObject*, PyObject*, int);
+    PyAPI_FUNC(PyObject*) PyObject_GetAttrString(PyObject*, const char*);
+    PyAPI_FUNC(int) PyObject_SetAttrString(PyObject*, const char*, PyObject *);
+    PyAPI_FUNC(int) PyObject_HasAttrString(PyObject*, const char*);
+    PyAPI_FUNC(PyObject*) PyObject_GetAttr(PyObject*, PyObject*);
+    PyAPI_FUNC(int) PyObject_SetAttr(PyObject*, PyObject*, PyObject*);
+    PyAPI_FUNC(int) PyObject_HasAttr(PyObject*, PyObject*);*/
+}
---
+++ src/CSnakes.Runtime/CPython/Structures.cs
@@ -0,0 +1,21 @@
+﻿using System.Runtime.InteropServices;
+
+
+namespace CSnakes.Runtime;
+
+using Py_ssize_t = System.IntPtr;
+
+internal unsafe partial class CPythonAPI
+{
+    struct PyObject
+    {
+        Py_ssize_t ob_refcnt;
+        PyObject* ob_type;
+    }
+
+    struct PyVarObject
+    {
+        PyObject ob_base;
+        Py_ssize_t ob_size;
+    }
+}

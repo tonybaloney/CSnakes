f7b7d36e8676581703cb60e4eb5f259dc4d2649e
Anthony Shaw <anthony.p.shaw@gmail.com>
2024-08-07 12:49:10 +1000

    Add exception APIs

---
+++ src/CSnakes.Runtime/CPython/Exceptions.cs
@@ -0,0 +1,18 @@
+﻿using System.Runtime.InteropServices;
+
+namespace CSnakes.Runtime.CPython;
+
+internal unsafe partial class CPythonAPI
+{
+
+    // Borrowed reference
+    [LibraryImport(PythonLibraryName)]
+    internal static partial nint PyErr_Occurred();
+
+    [LibraryImport(PythonLibraryName)]
+    internal static partial void PyErr_Clear();
+
+    // https://docs.python.org/3.10/c-api/exceptions.html#c.PyErr_Fetch
+    [LibraryImport(PythonLibraryName)]
+    internal static partial void PyErr_Fetch(out nint ptype, out nint pvalue, out nint ptraceback);
+}

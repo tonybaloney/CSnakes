d665a212315a9af7f870340941ffdfa1534e6768
Anthony Shaw <anthony.p.shaw@gmail.com>
2024-08-15 21:33:47 +1000

    Move the log statements

--- samples/Aspire/python/weather.py
+++ samples/Aspire/python/weather.py
@@ -7,14 +7,13 @@ import logging
 summaries: List[str] = ["Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"]
 
 logging.basicConfig(level=logging.INFO)
-
 tracer = configure_oltp_grpc_tracing()
-
 logger = logging.getLogger(__name__)
 
+
 def get_weather_forecast(trace_id: str = None, span_id: str = None) -> List[Dict[str, Any]]:
-    logger.info("Generating weather forecast from Python")
     with tracer.start_as_current_span("generate-forecast", get_span_context(trace_id, span_id)):
+        logger.info("Generating weather forecast from Python")
         temperature_c = random.randint(-20, 55)
         forecast: List[Dict[str, Any]] = [
             {
@@ -25,6 +24,6 @@ def get_weather_forecast(trace_id: str = None, span_id: str = None) -> List[Dict
             for index in range(1, 6)
         ]
 
-    logger.info("Generated forecast: %s", forecast)
+        logger.info("Generated forecast: %s", forecast)
 
     return forecast

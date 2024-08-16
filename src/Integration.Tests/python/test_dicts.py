fbdcebc515b9f445b1bea9bb3977691fb51b7140
Anthony Shaw <anthony.p.shaw@gmail.com>
2024-08-16 16:23:46 +1000

    Remove my fancy Python 3.12 syntax

--- src/Integration.Tests/python/test_dicts.py
+++ src/Integration.Tests/python/test_dicts.py
@@ -10,21 +10,22 @@ def test_dict_str_list_int(a: dict[str, list[int]]) -> dict[str, list[int]]:
 def test_dict_str_dict_int(a: dict[str, dict[str, int]]) -> dict[str, dict[str, int]]:
     return a
 
-class MyMappingType[K, V] (Mapping[K, V]):
-    def __init__(self, a: Mapping[K, V]):
+class MyMappingType:
+    def __init__(self, a):
         self.actual_dict = dict(a)
 
-    def __getitem__(self, key: K) -> V:
+    def __getitem__(self, key):
         return self.actual_dict[key]
 
-    def items(self) -> ItemsView[K, V]:
+    def items(self) -> ItemsView:
         return self.actual_dict.items()
 
     def __len__(self) -> int:
         return self.actual_dict.__len__()
 
-    def __iter__(self) -> Iterator[K]:
-        return super().__iter__()
+    def __iter__(self) -> Iterator:
+        return self.actual_dict.__iter__()
+
 
 def test_mapping(a: Mapping[str, int]) -> Mapping[str, int]:
     return MyMappingType(a)

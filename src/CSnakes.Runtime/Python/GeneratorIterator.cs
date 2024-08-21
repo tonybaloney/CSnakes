b880e66f76b9279ad9679ad812866f40bbbae1d7
Aaron Powell <me@aaron-powell.com>
2024-08-21 11:43:19 +1000

    Update src/CSnakes.Runtime/Python/GeneratorIterator.cs

--- src/CSnakes.Runtime/Python/GeneratorIterator.cs
+++ src/CSnakes.Runtime/Python/GeneratorIterator.cs
@@ -65,7 +65,8 @@ public class GeneratorIterator<TYield, TSend, TReturn>(PyObject generator) : IGe
         }
         catch (PythonInvocationException pyO) when (pyO.PythonExceptionType == "StopIteration")
         {
-            return true;
+            throw new ArgumentOutOfRangeException("Generator is exhausted");
+
 
         }
 

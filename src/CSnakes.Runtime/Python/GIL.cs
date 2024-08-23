89f712bbcfe29e82dc72337fb65afde2159c0872
Anthony Shaw <anthony.p.shaw@gmail.com>
2024-08-23 19:19:15 +1000

    Makes the GIL state a single instance per thread and also makes it recursive to avoid releasing and re-acquiring the GIL when it isn't needed. Implements #164

--- src/CSnakes.Runtime/Python/GIL.cs
+++ src/CSnakes.Runtime/Python/GIL.cs
@@ -5,25 +5,50 @@ namespace CSnakes.Runtime.Python;
 
 public static class GIL
 {
+    [ThreadStatic] private static PyGilState? currentState;
+
     internal class PyGilState : IDisposable
     {
-        private readonly nint gilState;
+        private nint gilState;
+        private int recursionCount;
 
         public PyGilState()
         {
             Debug.Assert(CPythonAPI.IsInitialized);
             gilState = CPythonAPI.PyGILState_Ensure();
+            recursionCount = 1;
+        }
+
+        public void Ensure()
+        {
+            if (recursionCount == 0)
+            {
+                gilState = CPythonAPI.PyGILState_Ensure();
+            }
+            recursionCount++;
         }
 
         public void Dispose()
         {
+            recursionCount--;
+            if (recursionCount > 0)
+            {
+                return;
+            }
             CPythonAPI.PyGILState_Release(gilState);
         }
     }
 
     public static IDisposable Acquire()
     {
-        // TODO: Decide if we want access to the `ILogger` here to pass to the `PyGilState` constructor
-        return new PyGilState();
+        if (currentState == null)
+        {
+            currentState = new PyGilState();
+        }
+        else
+        {
+            currentState.Ensure();
+        }
+        return currentState;
     }
 }

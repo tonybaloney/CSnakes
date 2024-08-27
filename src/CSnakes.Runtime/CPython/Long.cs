fa174a2adc228226c272998945f8ca6bcce6b156
Anthony Shaw <anthony.p.shaw@gmail.com>
2024-08-27 15:55:22 +1000

    Do error checks inside Python interop classes

--- src/CSnakes.Runtime/CPython/Long.cs
+++ src/CSnakes.Runtime/CPython/Long.cs
@@ -13,11 +13,41 @@ internal unsafe partial class CPythonAPI
     [LibraryImport(PythonLibraryName)]
     internal static partial nint PyLong_FromLongLong(long v);
 
-    [LibraryImport(PythonLibraryName)]
-    internal static partial long PyLong_AsLongLong(PyObject p);
+    /// <summary>
+    /// Calls PyLong_AsLongLong and throws a Python Exception if an error occurs.
+    /// </summary>
+    /// <param name="p"></param>
+    /// <returns></returns>
+    internal static long PyLong_AsLongLong(PyObject p)
+    {
+        long result = PyLong_AsLongLong_(p);
+        if (result == -1 && PyErr_Occurred())
+        {
+            throw PyObject.ThrowPythonExceptionAsClrException("Error converting Python object to int, check that the object was a Python int or that the value wasn't too large. See InnerException for details.");
+        }
+        return result;
+    }
 
-    [LibraryImport(PythonLibraryName)]
-    internal static partial int PyLong_AsLong(PyObject p);
+    [LibraryImport(PythonLibraryName, EntryPoint = "PyLong_AsLongLong")]
+    private static partial long PyLong_AsLongLong_(PyObject p);
+
+    /// <summary>
+    /// Calls PyLong_AsLong and throws a Python Exception if an error occurs.
+    /// </summary>
+    /// <param name="p"></param>
+    /// <returns></returns>
+    internal static long PyLong_AsLong(PyObject p)
+    {
+        long result = PyLong_AsLong_(p);
+        if (result == -1 && PyErr_Occurred())
+        {
+            throw PyObject.ThrowPythonExceptionAsClrException("Error converting Python object to int, check that the object was a Python int or that the value wasn't too large. See InnerException for details.");
+        }
+        return result;
+    }
+
+    [LibraryImport(PythonLibraryName, EntryPoint = "PyLong_AsLong")]
+    private static partial int PyLong_AsLong_(PyObject p);
 
     internal static bool IsPyLong(PyObject p)
     {

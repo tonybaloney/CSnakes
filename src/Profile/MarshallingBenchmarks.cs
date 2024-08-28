c4880ce469c698efe34681d40f9e6cdc90d58711
Anthony Shaw <anthony.p.shaw@gmail.com>
2024-08-28 14:51:08 +1000

    refactor the profile into a benchmark

---
+++ src/Profile/MarshallingBenchmarks.cs
@@ -0,0 +1,35 @@
+﻿using CSnakes.Runtime;
+using BenchmarkDotNet.Attributes;
+
+
+namespace Profile;
+
+[MarkdownExporter]
+public class MarshallingBenchmarks: BaseBenchmark
+{
+    private IMarshallingBenchmarks? mod ;
+
+    [GlobalSetup]
+    public void Setup()
+    {
+        mod = Env.MarshallingBenchmarks();
+    }
+
+    [Benchmark]
+    public void ComplexReturn()
+    {
+        mod!.GenerateData(5, "hello", (3.2, "testinput"), false);
+    }
+
+    [Benchmark]
+    public void ComplexReturnLazy()
+    {
+        mod!.GenerateDataAny(5, "hello", (3.2, "testinput"), false);
+    }
+
+    [Benchmark]
+    public void EmptyFunction()
+    {
+        mod!.EmptyFunction();
+    }
+}

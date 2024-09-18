d45b2a0ff52f665aa6305b1d7c866fb9eab507de
Atif Aziz <code@raboof.com>
2024-09-18 14:06:14 +0200

    Revert "Use function pointers interface of static interfaces"
    This reverts commit d584188675c6c56c5dea392cfbc24fdcd8bddbba.

--- src/CSnakes.Runtime/Python/Pack.cs
+++ src/CSnakes.Runtime/Python/Pack.cs
@@ -5,21 +5,47 @@ using System.Runtime.InteropServices.Marshalling;
 namespace CSnakes.Runtime.Python;
 
 /// <summary>
-/// These methods are used internally to create a PyObject where the Dispose() call will dispose all items in
+/// These methods are used internally to create a PyObject where the Dispose() call will dispose all items in 
 /// the collection inside the same call stack. This avoids the .NET GC Finalizer thread from disposing the items
 /// that were created and creating a GIL contention issue when other code is running.
 /// </summary>
 internal static class Pack
 {
-    internal static unsafe PyObject CreateTuple(Span<PyObject> items) =>
-        PyObject.Create(CreateListOrTuple(items, &CPythonAPI.PyTuple_New, &CPythonAPI.PyTuple_SetItemRaw));
+    internal static PyObject CreateTuple(Span<PyObject> items) =>
+        PyObject.Create(CreateListOrTuple<TupleBuilder>(items));
 
-    internal static unsafe PyObject CreateList(Span<PyObject> items) =>
-        PyObject.Create(CreateListOrTuple(items, &CPythonAPI.PyList_New, &CPythonAPI.PyList_SetItemRaw));
+    internal static PyObject CreateList(Span<PyObject> items) =>
+        PyObject.Create(CreateListOrTuple<ListBuilder>(items));
 
-    public static unsafe nint CreateListOrTuple(Span<PyObject> items,
-                                                delegate* <nint, nint> newObject,
-                                                delegate* <nint, nint, nint, int> setItemRaw)
+    public interface IListOrTupleBuilder
+    {
+        static abstract nint New(nint size);
+        static abstract int SetItemRaw(nint ob, nint pos, nint o);
+    }
+
+    public sealed class ListBuilder : IListOrTupleBuilder
+    {
+        // As per Python/C API docs for `PyList_New`:
+        //
+        // > If len is greater than zero, the returned list object's items are set to `NULL`. Thus
+        // > you cannot use abstract API functions such as `PySequence_SetItem()` or expose the
+        // > object to Python code before setting all items to a real object with
+        // > `PyList_SetItem()`.
+        //
+        // Source: https://docs.python.org/3/c-api/list.html#c.PyList_New
+
+        public static IntPtr New(IntPtr size) => CPythonAPI.PyList_New(size);
+        public static int SetItemRaw(IntPtr ob, IntPtr pos, IntPtr o) => CPythonAPI.PyList_SetItemRaw(ob, pos, o);
+    }
+
+    public sealed class TupleBuilder : IListOrTupleBuilder
+    {
+        public static IntPtr New(IntPtr size) => size == 0 ? CPythonAPI.GetPyEmptyTuple() : CPythonAPI.PyTuple_New(size);
+        public static int SetItemRaw(IntPtr ob, IntPtr pos, IntPtr o) => CPythonAPI.PyTuple_SetItemRaw(ob, pos, o);
+    }
+
+    public static nint CreateListOrTuple<TBuilder>(Span<PyObject> items)
+        where TBuilder : IListOrTupleBuilder
     {
         nint obj = 0;
 
@@ -44,12 +70,12 @@ internal static class Pack
 
             Debug.Assert(uninitializedHandles.Length == 0);
 
-            obj = newObject(items.Length);
+            obj = TBuilder.New(items.Length);
 
             var i = 0;
             foreach (var handle in handles)
             {
-                int result = setItemRaw(obj, i++, handle);
+                int result = TBuilder.SetItemRaw(obj, i++, handle);
                 if (result == -1)
                 {
                     throw PyObject.ThrowPythonExceptionAsClrException();

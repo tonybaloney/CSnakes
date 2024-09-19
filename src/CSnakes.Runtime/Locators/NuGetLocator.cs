04fe6896972935e8e1c8fcb86d9f9f202375daef
Igor Velikorossov <igor.velikorossov@microsoft.com>
2024-09-20 09:21:03 +1000

    fixup! Fix test logging

--- src/CSnakes.Runtime/Locators/NuGetLocator.cs
+++ src/CSnakes.Runtime/Locators/NuGetLocator.cs
@@ -11,7 +11,11 @@ internal class NuGetLocator(string nugetVersion, Version version) : PythonLocato
             {
                 (NuGetPackages : { Length: > 0 } path, _) => path,
                 (_, UserProfile: { Length: > 0 } path) => Path.Combine(path, ".nuget", "packages"),
+#if NET9_0_OR_GREATER
                 _ => throw new DirectoryNotFoundException("Neither NUGET_PACKAGES or USERPROFILE environments variable were found, which are needed to locate the NuGet package cache.")
+#else
+                _ => throw new Exception("Neither NUGET_PACKAGES or USERPROFILE environments variable were found, which are needed to locate the NuGet package cache.")
+#endif
             };
         // TODO : Load optional path from nuget settings. https://learn.microsoft.com/en-us/nuget/consume-packages/managing-the-global-packages-and-cache-folders
         string nugetPath = Path.Combine(globalNugetPackagesPath, "python", nugetVersion, "tools");

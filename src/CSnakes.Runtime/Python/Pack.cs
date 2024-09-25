5423d6ba7cfc773a4b8a16d271f7cfa7592b006d
Atif Aziz <code@raboof.com>
2024-09-26 00:29:09 +0200

    Pre-allocate list based on source length (#206)

--- src/CSnakes.Runtime/Python/Pack.cs
+++ src/CSnakes.Runtime/Python/Pack.cs
@@ -1,5 +1,8 @@
 ﻿using CSnakes.Runtime.CPython;
-using System.Runtime.InteropServices.Marshalling;
+using System.Diagnostics;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using PyObjectMarshaller = System.Runtime.InteropServices.Marshalling.SafeHandleMarshaller<CSnakes.Runtime.Python.PyObject>;
 
 namespace CSnakes.Runtime.Python;
 
@@ -10,46 +13,155 @@ namespace CSnakes.Runtime.Python;
 /// </summary>
 internal static class Pack
 {
-    internal static PyObject CreateTuple(Span<PyObject> items)
-    {
-        List<SafeHandleMarshaller<PyObject>.ManagedToUnmanagedIn> marshallers = new(items.Length);
-        try
-        {
-            var handles = items.Length < 18 // .NET tuples are max 17 items. This is a performance optimization.
-                ? stackalloc IntPtr[items.Length]
-                : new IntPtr[items.Length];
+    internal static PyObject CreateTuple(Span<PyObject> items) =>
+        PyObject.Create(CreateListOrTuple<TupleBuilder>(items));
 
-            for (int i = 0; i < items.Length; i++)
-            {
-                SafeHandleMarshaller<PyObject>.ManagedToUnmanagedIn m = default;
-                m.FromManaged(items[i]);
-                marshallers.Add(m);
-                handles[i] = m.ToUnmanaged();
-            }
-            return PyObject.Create(CPythonAPI.PackTuple(handles));
-        }
-        finally
-        {
-            foreach (var m in marshallers)
-            {
-                m.Free();
-            }
-        }
+    internal static PyObject CreateList(Span<PyObject> items) =>
+        PyObject.Create(CreateListOrTuple<ListBuilder>(items));
+
+    private interface IListOrTupleBuilder
+    {
+        static abstract nint New(nint size);
+        static abstract int SetItemRaw(nint ob, nint pos, nint o);
     }
 
-    internal static PyObject CreateList(Span<PyObject> items)
+    private sealed class ListBuilder : IListOrTupleBuilder
     {
-        PyObject pyList = PyObject.Create(CPythonAPI.PyList_New(0)); // TODO: preallocate based on items.Length and use PyList_SetItem
+        private ListBuilder() { }
+
+        // As per Python/C API docs for `PyList_New`:
+        //
+        // > If len is greater than zero, the returned list object's items are set to `NULL`. Thus
+        // > you cannot use abstract API functions such as `PySequence_SetItem()` or expose the
+        // > object to Python code before setting all items to a real object with
+        // > `PyList_SetItem()`.
+        //
+        // Source: https://docs.python.org/3/c-api/list.html#c.PyList_New
+
+        public static IntPtr New(IntPtr size) => CPythonAPI.PyList_New(size);
+        public static int SetItemRaw(IntPtr ob, IntPtr pos, IntPtr o) => CPythonAPI.PyList_SetItemRaw(ob, pos, o);
+    }
+
+    private sealed class TupleBuilder : IListOrTupleBuilder
+    {
+        private TupleBuilder() { }
+        public static IntPtr New(IntPtr size) => size == 0 ? CPythonAPI.GetPyEmptyTuple() : CPythonAPI.PyTuple_New(size);
+        public static int SetItemRaw(IntPtr ob, IntPtr pos, IntPtr o) => CPythonAPI.PyTuple_SetItemRaw(ob, pos, o);
+    }
+
+    const int FixedArrayLength = 8;
+
+    [InlineArray(FixedArrayLength)]
+    private struct ArrayOf8<T>
+    {
+        private T _;
+    }
+
+    private static nint CreateListOrTuple<TBuilder>(Span<PyObject> items)
+        where TBuilder : IListOrTupleBuilder
+    {
+        // Allocate initial space for the handles and marshallers on the stack.
+        // If the number of items exceeds the stack space, allocate and spill
+        // the rest into an array on the heap.
+        // TODO Consider using an array pool for the spilled handles and marshallers.
+
+        const int stackSpillThreshold = FixedArrayLength;
+        var spillLength = Math.Max(0, items.Length - stackSpillThreshold);
+
+        Span<nint> initialHandles = stackalloc nint[Math.Min(stackSpillThreshold, items.Length)];
+        nint[]? spilledHandles = spillLength > 0 ? new nint[spillLength] : null;
+
+        var initialMarshallers = new ArrayOf8<PyObjectMarshaller.ManagedToUnmanagedIn>();
+        PyObjectMarshaller.ManagedToUnmanagedIn[]? spilledMarshallers =
+            spillLength > 0
+            ? new PyObjectMarshaller.ManagedToUnmanagedIn[spillLength]
+            : null;
+
+        var uninitializedMarshallers = MemoryMarshal.CreateSpan(ref Unsafe.As<ArrayOf8<PyObjectMarshaller.ManagedToUnmanagedIn>, PyObjectMarshaller.ManagedToUnmanagedIn>(ref initialMarshallers), stackSpillThreshold);
+        var uninitializedHandles = initialHandles;
+
+        // The following loop initializes the marshallers and handles for each
+        // item in the input span. It is assumed that no exceptions are thrown
+        // during this loop. The marshallers are freed in the finally block of
+        // the actual list/tuple initialization.
 
         foreach (var item in items)
         {
-            int result = CPythonAPI.PyList_Append(pyList, item);
-            if (result == -1)
+            PyObjectMarshaller.ManagedToUnmanagedIn m = default;
+            m.FromManaged(item);
+
+            if (uninitializedMarshallers.IsEmpty)
             {
-                throw PyObject.ThrowPythonExceptionAsClrException();
+                Debug.Assert(spilledMarshallers is not null);
+                uninitializedMarshallers = spilledMarshallers;
             }
+
+            uninitializedMarshallers[0] = m;
+            uninitializedMarshallers = uninitializedMarshallers[1..];
+
+            if (uninitializedHandles.IsEmpty)
+            {
+                Debug.Assert(spilledHandles is not null);
+                uninitializedHandles = spilledHandles;
+            }
+
+            uninitializedHandles[0] = m.ToUnmanaged();
+            uninitializedHandles = uninitializedHandles[1..];
         }
 
-        return pyList;
+        nint obj = 0;
+
+        try
+        {
+            obj = TBuilder.New(items.Length);
+            SetItems(obj, spilledHandles, SetItems(obj, initialHandles, 0));
+
+            return obj;
+
+            static int SetItems(nint obj, Span<nint> handles, int i)
+            {
+                foreach (var handle in handles)
+                {
+                    int result = TBuilder.SetItemRaw(obj, i++, handle);
+                    if (result == -1)
+                    {
+                        throw PyObject.ThrowPythonExceptionAsClrException();
+                    }
+                }
+
+                return i;
+            }
+        }
+        catch
+        {
+            if (obj != 0)
+            {
+                CPythonAPI.Py_DecRefRaw(obj);
+            }
+
+            throw;
+        }
+        finally
+        {
+            if (spilledMarshallers is null)
+            {
+                foreach (var m in initialMarshallers[..items.Length])
+                {
+                    m.Free();
+                }
+            }
+            else
+            {
+                foreach (var m in initialMarshallers)
+                {
+                    m.Free();
+                }
+
+                foreach (var m in spilledMarshallers)
+                {
+                    m.Free();
+                }
+            }
+        }
     }
 }

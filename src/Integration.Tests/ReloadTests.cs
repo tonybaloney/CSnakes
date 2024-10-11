efb00532ecd04788094bfc07106b978a234c0dd1
Anthony Shaw <anthony.p.shaw@gmail.com>
2024-10-11 19:41:56 +1100

    Change the test names

---
+++ src/Integration.Tests/ReloadTests.cs
@@ -0,0 +1,19 @@
+﻿namespace Integration.Tests;
+
+public class ReloadTests(PythonEnvironmentFixture fixture) : IntegrationTestBase(fixture)
+{
+    [Fact]
+    public void TestModule_Reload()
+    {
+        var testModule = Env.TestChanges();
+        Assert.Equal(52, testModule.TestNumber());
+        // Change test_changes.py to return 42
+        File.WriteAllText(
+            Path.Join(Environment.CurrentDirectory, "python", "test_changes.py"),
+            "def test_number() -> int:\n    return 42\n"
+        );
+
+        testModule.ReloadModule();
+        Assert.Equal(42, testModule.TestNumber());
+    }
+}
--- src/Integration.Tests/TestDependency.cs
+++ src/Integration.Tests/TestDependency.cs
@@ -7,19 +7,4 @@ public class TestDependency(PythonEnvironmentFixture fixture) : IntegrationTestB
     {
         Assert.True(Env.TestDependency().TestNothing());
     }
-
-    [Fact]
-    public void TestModule_Reload()
-    {
-        var testModule = Env.TestChanges();
-        Assert.Equal(52, testModule.TestNumber());
-        // Change test_changes.py to return 42
-        File.WriteAllText(
-            Path.Join(Environment.CurrentDirectory, "python", "test_changes.py"),
-            "def test_number() -> int:\n    return 42\n"
-        );
-
-        testModule.ReloadModule();
-        Assert.Equal(42, testModule.TestNumber());
-    }
 }

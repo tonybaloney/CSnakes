9515c33d90fd9f1d3877bcc268174d51973544ca
Atif Aziz <code@raboof.com>
2024-10-12 19:25:32 +0200

    Fix .NET 9 references which are not available via enum yet

--- src/CSnakes.Tests/GeneratedSignatureTests.cs
+++ src/CSnakes.Tests/GeneratedSignatureTests.cs
@@ -59,7 +59,7 @@ public class GeneratedSignatureTests(TestEnvironment testEnv) : IClassFixture<Te
 #if NET8_0
             .WithReferenceAssemblies(ReferenceAssemblyKind.Net80)
 #elif NET9_0
-            .WithReferenceAssemblies(ReferenceAssemblyKind.Net90)
+            .WithReferenceAssemblies(Net90.References.All)
 #else
 #error Unsupported .NET tareget
 #endif

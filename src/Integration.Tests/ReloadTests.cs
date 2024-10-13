5bc8588aefa92bd915faae9ec28ec81c16753f6d
Anthony Shaw <anthony.p.shaw@gmail.com>
2024-10-14 08:06:13 +1100

    Write some tests for different scenarios

--- src/Integration.Tests/ReloadTests.cs
+++ src/Integration.Tests/ReloadTests.cs
@@ -2,14 +2,26 @@
 
 public class ReloadTests(PythonEnvironmentFixture fixture) : IntegrationTestBase(fixture)
 {
+    private const string originalCode = """
+def test_number() -> int:
+    return 52
+""";
+    private static void ResetCode() => ReloadTests.ResetCode(originalCode);
+
+    private static void ResetCode(string code) => File.WriteAllText(
+        Path.Join(Environment.CurrentDirectory, "python", "test_reload.py"),
+        code
+    );
+
     [Fact]
     public void TestModule_Reload()
     {
+        ResetCode();
         var testModule = Env.TestReload();
+        testModule.ReloadModule();
         Assert.Equal(52, testModule.TestNumber());
         // Change test_reload.py to return 42
-        File.WriteAllText(
-            Path.Join(Environment.CurrentDirectory, "python", "test_reload.py"),
+        ResetCode(
             """
             def test_number() -> int:
                 return 42
@@ -19,5 +31,46 @@ public class ReloadTests(PythonEnvironmentFixture fixture) : IntegrationTestBase
 
         testModule.ReloadModule();
         Assert.Equal(42, testModule.TestNumber());
+    }
+
+    [Fact]
+    public void TestModule_ReloadDeleteFunction()
+    {
+        ResetCode();
+        var testModule = Env.TestReload();
+        testModule.ReloadModule();
+        Assert.Equal(52, testModule.TestNumber());
+        // Change test_reload.py to return 42
+        ResetCode(
+            """
+            def _test_number() -> int:
+                return 42
+
+            """
+        );
+
+        testModule.ReloadModule();
+        Assert.Equal(52, testModule.TestNumber());
+    }
+
+    [Fact]
+    public void TestModule_ReloadChangeSignature()
+    {
+        ResetCode();
+        var testModule = Env.TestReload();
+        testModule.ReloadModule();
+
+        Assert.Equal(52, testModule.TestNumber());
+        // Change test_reload.py to return 42
+        ResetCode(
+            """
+            def test_number() -> str:
+                return "42"
+
+            """
+        );
+
+        testModule.ReloadModule();
+        Assert.Throws<PythonInvocationException>(() => testModule.TestNumber());
     }
 }

f5534c6c117434e15bf78d9822fe256f96773ffe
Atif Aziz <code@raboof.com>
2024-10-14 08:39:42 +0200

    Have "ReloadModule" inherited/implemented through an interface (#269)
    See also: <https://github.com/tonybaloney/CSnakes/pull/259#discussion_r1797715811>

--- src/Integration.Tests/python/test_reload.py
+++ src/Integration.Tests/python/test_reload.py
@@ -1,3 +1,15 @@
 
 def test_number() -> int:
     return 52
+
+# The following function will introduce a method named `ReloadModule` in the C#
+# proxy's interface that hides an identically named and inherited method
+# from `IReloadableModuleImport`. The source generator should appropriately add
+# the `new` keyword so the following warning is never emitted:
+#
+# > warning CS0108: 'ITestReload.ReloadModule()' hides inherited member
+# >   'IReloadableModuleImport.ReloadModule()'. Use the new keyword if hiding
+# >   was intended.
+
+def reload_module():
+    pass

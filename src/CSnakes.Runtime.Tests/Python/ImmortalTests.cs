bd84bca2048b83a76ed2eba8e0f595db936b7832
Anthony Shaw <anthony.p.shaw@gmail.com>
2024-10-15 19:46:12 +1100

    Readd the tests

--- src/CSnakes.Runtime.Tests/Python/ImmortalTests.cs
+++ src/CSnakes.Runtime.Tests/Python/ImmortalTests.cs
@@ -3,38 +3,38 @@
 namespace CSnakes.Runtime.Tests.Python;
 public class ImmortalTests : RuntimeTestBase
 {
-    //[Fact]
-    //public void TestZero()
-    //{
-    //    Assert.Equal("0", PyObject.Zero.ToString());
-    //    Assert.Equal("0", PyObject.Zero.GetRepr());
-    //    Assert.False(PyObject.Zero.IsNone());
-    //    Assert.Equal(PyObject.Zero, PyObject.Zero.Clone());
-    //    using var zero = PyObject.From(0);
-    //    Assert.Equal(PyObject.Zero, zero);
-    //}
+    [Fact]
+    public void TestZero()
+    {
+        Assert.Equal("0", PyObject.Zero.ToString());
+        Assert.Equal("0", PyObject.Zero.GetRepr());
+        Assert.False(PyObject.Zero.IsNone());
+        Assert.Equal(PyObject.Zero, PyObject.Zero.Clone());
+        using var zero = PyObject.From(0);
+        Assert.Equal(PyObject.Zero, zero);
+    }
 
-    //[Fact]
-    //public void TestNegativeOne()
-    //{
-    //    Assert.Equal("-1", PyObject.NegativeOne.ToString());
-    //    Assert.Equal("-1", PyObject.NegativeOne.GetRepr());
-    //    Assert.False(PyObject.NegativeOne.IsNone());
-    //    Assert.Equal(PyObject.NegativeOne, PyObject.NegativeOne.Clone());
-    //    using var negativeOne = PyObject.From(-1);
-    //    Assert.Equal(PyObject.NegativeOne, negativeOne);
-    //}
+    [Fact]
+    public void TestNegativeOne()
+    {
+        Assert.Equal("-1", PyObject.NegativeOne.ToString());
+        Assert.Equal("-1", PyObject.NegativeOne.GetRepr());
+        Assert.False(PyObject.NegativeOne.IsNone());
+        Assert.Equal(PyObject.NegativeOne, PyObject.NegativeOne.Clone());
+        using var negativeOne = PyObject.From(-1);
+        Assert.Equal(PyObject.NegativeOne, negativeOne);
+    }
 
-    //[Fact]
-    //public void TestOne()
-    //{
-    //    Assert.Equal("1", PyObject.One.ToString());
-    //    Assert.Equal("1", PyObject.One.GetRepr());
-    //    Assert.False(PyObject.One.IsNone());
-    //    Assert.Equal(PyObject.One, PyObject.One.Clone());
-    //    using var one = PyObject.From(1);
-    //    Assert.Equal(PyObject.One, one);
-    //}
+    [Fact]
+    public void TestOne()
+    {
+        Assert.Equal("1", PyObject.One.ToString());
+        Assert.Equal("1", PyObject.One.GetRepr());
+        Assert.False(PyObject.One.IsNone());
+        Assert.Equal(PyObject.One, PyObject.One.Clone());
+        using var one = PyObject.From(1);
+        Assert.Equal(PyObject.One, one);
+    }
 
     [Fact]
     public void TestFalse()

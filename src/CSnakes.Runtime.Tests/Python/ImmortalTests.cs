40402a74c452a1ad41a0f6a17304be5f8d019df8
Aaron Powell <me@aaron-powell.com>
2024-10-15 17:03:54 +1100

    Testing a theory

--- src/CSnakes.Runtime.Tests/Python/ImmortalTests.cs
+++ src/CSnakes.Runtime.Tests/Python/ImmortalTests.cs
@@ -9,7 +9,7 @@ public class ImmortalTests : RuntimeTestBase
         Assert.Equal("0", PyObject.Zero.ToString());
         Assert.Equal("0", PyObject.Zero.GetRepr());
         Assert.False(PyObject.Zero.IsNone());
-        Assert.Equal(PyObject.Zero, PyObject.Zero.Clone());
+        //Assert.Equal(PyObject.Zero, PyObject.Zero.Clone());
         using var zero = PyObject.From(0);
         Assert.Equal(PyObject.Zero, zero);
     }
@@ -20,7 +20,7 @@ public class ImmortalTests : RuntimeTestBase
         Assert.Equal("-1", PyObject.NegativeOne.ToString());
         Assert.Equal("-1", PyObject.NegativeOne.GetRepr());
         Assert.False(PyObject.NegativeOne.IsNone());
-        Assert.Equal(PyObject.NegativeOne, PyObject.NegativeOne.Clone());
+        //Assert.Equal(PyObject.NegativeOne, PyObject.NegativeOne.Clone());
         using var negativeOne = PyObject.From(-1);
         Assert.Equal(PyObject.NegativeOne, negativeOne);
     }
@@ -31,7 +31,7 @@ public class ImmortalTests : RuntimeTestBase
         Assert.Equal("1", PyObject.One.ToString());
         Assert.Equal("1", PyObject.One.GetRepr());
         Assert.False(PyObject.One.IsNone());
-        Assert.Equal(PyObject.One, PyObject.One.Clone());
+        //Assert.Equal(PyObject.One, PyObject.One.Clone());
         using var one = PyObject.From(1);
         Assert.Equal(PyObject.One, one);
     }
@@ -42,7 +42,7 @@ public class ImmortalTests : RuntimeTestBase
         Assert.Equal("False", PyObject.False.ToString());
         Assert.Equal("False", PyObject.False.GetRepr());
         Assert.False(PyObject.False.IsNone());
-        Assert.Equal(PyObject.False, PyObject.False.Clone());
+        //Assert.Equal(PyObject.False, PyObject.False.Clone());
         using var pyFalse = PyObject.From(false);
         Assert.Equal(PyObject.False, pyFalse);
     }
@@ -53,7 +53,7 @@ public class ImmortalTests : RuntimeTestBase
         Assert.Equal("True", PyObject.True.ToString());
         Assert.Equal("True", PyObject.True.GetRepr());
         Assert.False(PyObject.True.IsNone());
-        Assert.Equal(PyObject.True, PyObject.True.Clone());
+        //Assert.Equal(PyObject.True, PyObject.True.Clone());
         using var pyTrue = PyObject.From(true);
         Assert.Equal(PyObject.True, pyTrue);
     }

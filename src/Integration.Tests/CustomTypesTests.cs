b94cf46c08fb1e8b652734aa9c73f21efa1b76bc
Aaron Powell <me@aaron-powell.com>
2024-10-18 11:54:47 +1100

    Super hacky initial implementation

---
+++ src/Integration.Tests/CustomTypesTests.cs
@@ -0,0 +1,20 @@
+﻿namespace Integration.Tests;
+
+public class CustomTypesTests(PythonEnvironmentFixture fixture) : IntegrationTestBase(fixture)
+{
+    [Fact]
+    public void CreatePerson()
+    {
+        var testModule = Env.TestCustomClass();
+        var person = testModule.CreatePerson("Jane", "Doe").As<Person>();
+        Assert.Equal("Jane", person.FirstName);
+        Assert.Equal("Doe", person.Surname);
+    }
+}
+
+public class Person
+{
+    public required string FirstName { get; set; }
+    [PythonName("last_name")]
+    public required string Surname { get; set; }
+}

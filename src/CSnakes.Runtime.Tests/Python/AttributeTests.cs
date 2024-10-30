f052d92955872d95602d6d0c8a92c8bfe687f01f
CaptainWelpe <michael.keller@minesworld.de>
2024-10-30 22:13:03 +0100

    test for https://github.com/tonybaloney/CSnakes/issues/302

---
+++ src/CSnakes.Runtime.Tests/Python/AttributeTests.cs
@@ -0,0 +1,27 @@
+﻿using CSnakes.Runtime.Python;
+
+namespace CSnakes.Runtime.Tests.Python;
+public class AttributeTests : RuntimeTestBase
+{
+    [Fact]
+    public void TestHasAttrFile()
+    {
+        using (GIL.Acquire())
+        {
+            using PyObject sys = Import.ImportModule("sys");
+            Assert.NotNull(sys);
+            Assert.True(sys.HasAttr("__file__"));
+        }
+    }
+
+    [Fact]
+    public void TestHasAttrName()
+    {
+        using (GIL.Acquire())
+        {
+            using PyObject sys = Import.ImportModule("sys");
+            Assert.NotNull(sys);
+            Assert.True(sys.HasAttr("__name__"));
+        }
+    }
+}

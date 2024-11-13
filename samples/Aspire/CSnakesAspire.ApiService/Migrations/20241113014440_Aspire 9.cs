eaac5e51bd24039944d78126da1eb8dfc6aea68e
Anthony Shaw <anthony.p.shaw@gmail.com>
2024-11-13 12:46:18 +1100

    Update migrations for aspire 9

---
+++ samples/Aspire/CSnakesAspire.ApiService/Migrations/20241113014440_Aspire 9.cs
@@ -0,0 +1,82 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CSnakesAspire.ApiService.Migrations
+{
+    /// <inheritdoc />
+    public partial class Aspire9 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<float>(
+                name: "Wind",
+                table: "WeatherRecords",
+                type: "real",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "float");
+
+            migrationBuilder.AlterColumn<float>(
+                name: "TemperatureMinC",
+                table: "WeatherRecords",
+                type: "real",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "float");
+
+            migrationBuilder.AlterColumn<float>(
+                name: "TemperatureMaxC",
+                table: "WeatherRecords",
+                type: "real",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "float");
+
+            migrationBuilder.AlterColumn<float>(
+                name: "Precipitation",
+                table: "WeatherRecords",
+                type: "real",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "float");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "Wind",
+                table: "WeatherRecords",
+                type: "float",
+                nullable: false,
+                oldClrType: typeof(float),
+                oldType: "real");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "TemperatureMinC",
+                table: "WeatherRecords",
+                type: "float",
+                nullable: false,
+                oldClrType: typeof(float),
+                oldType: "real");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "TemperatureMaxC",
+                table: "WeatherRecords",
+                type: "float",
+                nullable: false,
+                oldClrType: typeof(float),
+                oldType: "real");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "Precipitation",
+                table: "WeatherRecords",
+                type: "float",
+                nullable: false,
+                oldClrType: typeof(float),
+                oldType: "real");
+        }
+    }
+}

5bf155ae82c49955f8496dde94af652cb27284be
Anthony Shaw <anthony.p.shaw@gmail.com>
2024-11-15 18:18:54 +1100

    Add a harder test

--- src/Integration.Tests/CoroutineTests.cs
+++ src/Integration.Tests/CoroutineTests.cs
@@ -8,4 +8,17 @@ public class CoroutineTests(PythonEnvironmentFixture fixture) : IntegrationTestB
         long result = await mod.TestCoroutine();
         Assert.Equal(5, result);
     }
+
+    [Fact]
+    public async Task MultipleCoroutineCalls()
+    {
+        var mod = Env.TestCoroutines();
+        var tasks = new List<Task<long>>();
+        for (int i = 0; i < 10; i++)
+        {
+            tasks.Add(mod.TestCoroutine());
+        }
+        var r = await Task.WhenAll(tasks);
+        Assert.All(r, x => Assert.Equal(5, x));
+    }
 }

09cf89f82226f944a58785c016a49c579ed2be87
Anthony Shaw <anthony.p.shaw@gmail.com>
2024-11-24 13:31:28 -0800

    return actual list

--- samples/simple/ExamplePythonDependency/fast_embed.py
+++ samples/simple/ExamplePythonDependency/fast_embed.py
@@ -3,8 +3,5 @@ from fastembed import TextEmbedding
 def generate_embeddings(documents: list[str]) -> list:
     embedding_model = TextEmbedding() #1
     embeddings_list = list(embedding_model.embed(documents)) #2
-    
-    return [
-        [1.23, 4.56, 7.89],
-        [9.87, 6.54, 3.21],
-    ]
+    return embeddings_list
+

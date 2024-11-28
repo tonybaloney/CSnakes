c21dc0f6f09f3daae7361ec3c9b07cc7673564ee
Anthony Shaw <anthony.p.shaw@gmail.com>
2024-11-28 15:21:40 +1100

    Test that exceptions are handled gracefully

--- src/Integration.Tests/CoroutineTests.cs
+++ src/Integration.Tests/CoroutineTests.cs
@@ -21,4 +21,15 @@ public class CoroutineTests(PythonEnvironmentFixture fixture) : IntegrationTestB
         var r = await Task.WhenAll(tasks);
         Assert.All(r, x => Assert.Equal(5, x));
     }
+
+    [Fact]
+    public async Task CoroutineRaisesException()
+    {
+        var mod = Env.TestCoroutines();
+        var task = mod.TestCoroutineRaisesException();
+        var exception = await Assert.ThrowsAsync<PythonInvocationException>(async () => await task);
+        Assert.NotNull(exception.InnerException);
+        Assert.Equal("This is a Python exception", exception.InnerException.Message);
+        Assert.Equal("ValueError", exception.PythonExceptionType);
+    }
 }
--- src/Integration.Tests/python/test_coroutines.py
+++ src/Integration.Tests/python/test_coroutines.py
@@ -5,3 +5,8 @@ import asyncio
 async def test_coroutine() -> Coroutine[int, None, None]:
     await asyncio.sleep(0.1)
     return 5
+
+
+async def test_coroutine_raises_exception() -> Coroutine[int, None, None]:
+    raise ValueError("This is a Python exception")
+

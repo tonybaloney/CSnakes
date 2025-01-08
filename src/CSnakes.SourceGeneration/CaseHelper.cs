6686c74ace65c08796c105dc5bc2a8cce8ca5fa0
jozefhornik <66019378+jozefhornik@users.noreply.github.com>
2025-01-08 08:22:37 +0100

    Changing generations of names with underscore (#320)
    Co-authored-by: developer <developer@local>
    Co-authored-by: Anthony Shaw <anthony.p.shaw@gmail.com>

--- src/CSnakes.SourceGeneration/CaseHelper.cs
+++ src/CSnakes.SourceGeneration/CaseHelper.cs
@@ -4,7 +4,7 @@
     {
         public static string ToPascalCase(this string snakeCase)
         {
-            return string.Join("", snakeCase.Split('_').Select(s => s.Length > 1 ? char.ToUpperInvariant(s[0]) + s.Substring(1) : "_"));
+            return string.Join("", snakeCase.Split('_').Select(s => s.Length > 1 ? char.ToUpperInvariant(s[0]) + s.Substring(1) : s.Length > 0 ? s : "_"));
         }
 
         public static string ToLowerPascalCase(this string snakeCase)

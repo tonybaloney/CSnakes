9b58edb10e4bacab97f94d915d863f82bcb1f73b
Anthony Shaw <anthony.p.shaw@gmail.com>
2025-01-08 17:46:16 +1100

    Ensure conda envs exists in testing (#321)
    * ensure conda envs exists in testing
    
    * Remove cleanup
    
    * Pin older miniconda
    
    * Add extra steps
    
    * Check conda activate
    
    * activate conda
    
    * Get system info
    
    * Update to latest
    
    * Remove clean
    
    * print conda env paths
    
    * bash formatting
    
    * error when venv doesn't exist
    
    * add python version
    
    * check paths
    
    * add extra safeguard
    
    * undo: add extra safeguard
    
    * Use the python version of the conda environment as an override for the installed conda python version
    
    * Use the Python version from the environment variable
    
    * remove debug step

--- src/CSnakes.Runtime/EnvironmentManagement/CondaEnvironmentManagement.cs
+++ src/CSnakes.Runtime/EnvironmentManagement/CondaEnvironmentManagement.cs
@@ -1,23 +1,26 @@
 ﻿using CSnakes.Runtime.Locators;
 using Microsoft.Extensions.Logging;
+using System.Runtime.InteropServices;
 
 namespace CSnakes.Runtime.EnvironmentManagement;
 #pragma warning disable CS9113 // Parameter is unread. There for future use.
-internal class CondaEnvironmentManagement(ILogger logger, string name, bool ensureExists, CondaLocator conda, string? environmentSpecPath) : IEnvironmentManagement
+internal class CondaEnvironmentManagement(ILogger logger, string name, bool ensureEnvironment, CondaLocator conda, string? environmentSpecPath, string? pythonEnvironmentVersion) : IEnvironmentManagement
 #pragma warning restore CS9113 // Parameter is unread.
 {
     ILogger IEnvironmentManagement.Logger => logger;
 
     public void EnsureEnvironment(PythonLocationMetadata pythonLocation)
     {
-        if (!ensureExists)
+        if (!ensureEnvironment)
             return;
-
 
         var fullPath = Path.GetFullPath(GetPath());
         if (!Directory.Exists(fullPath))
         {
             logger.LogError("Cannot find conda environment at {fullPath}.", fullPath);
+
+            throw new InvalidOperationException($"Cannot find conda environment at {fullPath}.");
+
             // TODO: Automate the creation of the conda environments. 
             //var result = conda.ExecuteCondaShellCommand($"env create -n {name} -f {environmentSpecPath}");
             //if (!result)
@@ -37,5 +40,20 @@ internal class CondaEnvironmentManagement(ILogger logger, string name, bool ensu
     {
         // TODO: Conda environments are not always in the same location. Resolve the path correctly. 
         return Path.Combine(conda.CondaHome, "envs", name);
+    }
+
+    public virtual string GetExtraPackagePath(PythonLocationMetadata location)
+    {
+        var envLibPath = string.Empty;
+        if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+            envLibPath = Path.Combine(GetPath(), "Lib", "site-packages");
+        else
+        {
+            Version version = string.IsNullOrEmpty(pythonEnvironmentVersion) ? location.Version : ServiceCollectionExtensions.ParsePythonVersion(pythonEnvironmentVersion);
+            string suffix = location.FreeThreaded ? "t" : "";
+            // If the environment has a different version to the version in conda.
+            envLibPath = Path.Combine(GetPath(), "lib", $"python{version.Major}.{version.Minor}{suffix}", "site-packages");
+        }
+        return envLibPath;
     }
 }

fdac82e025df54e1a1a034b7f06bf183919e863c
Anthony Shaw <anthony.p.shaw@gmail.com>
2025-02-10 14:08:13 +1100

    Update test version

--- src/Integration.Tests/IntegrationTestBase.cs
+++ src/Integration.Tests/IntegrationTestBase.cs
@@ -19,7 +19,7 @@ public sealed class PythonEnvironmentFixture : IDisposable
 
     public PythonEnvironmentFixture()
     {
-        string pythonVersionWindows = Environment.GetEnvironmentVariable("PYTHON_VERSION") ?? "3.12.4";
+        string pythonVersionWindows = Environment.GetEnvironmentVariable("PYTHON_VERSION") ?? "3.12.9";
         string pythonVersionMacOS = Environment.GetEnvironmentVariable("PYTHON_VERSION") ?? "3.12";
         string pythonVersionLinux = Environment.GetEnvironmentVariable("PYTHON_VERSION") ?? "3.12";
         bool freeThreaded = Environment.GetEnvironmentVariable("PYTHON_FREETHREADED") == "true";

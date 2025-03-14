637e37f83368400ab7157d2dc785b775d226823e
Anthony Shaw <anthony.p.shaw@gmail.com>
2025-03-14 18:53:14 +1100

    Fix version parsing and the URL for alpha releases

--- src/CSnakes.Tests/VersionParsingTests.cs
+++ src/CSnakes.Tests/VersionParsingTests.cs
@@ -8,6 +8,7 @@ public class VersionParsingTests
     public void TestParsingVersions() {
         Assert.Equal("3.12.3.0", ServiceCollectionExtensions.ParsePythonVersion("3.12.3").ToString());
         Assert.Equal("3.10.3.0", ServiceCollectionExtensions.ParsePythonVersion("3.10.3").ToString());
+        Assert.Equal("3.14.0.0", ServiceCollectionExtensions.ParsePythonVersion("3.14.0a5").ToString());
         Assert.Equal("3.14.0.0", ServiceCollectionExtensions.ParsePythonVersion("3.14.0-alpha.5").ToString());
     }
 }

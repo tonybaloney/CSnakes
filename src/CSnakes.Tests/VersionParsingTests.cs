829807976dc31f070736771060a2d2ff54f0e1e0
Anthony Shaw <anthony.p.shaw@gmail.com>
2025-03-14 18:44:43 +1100

    Add tests

---
+++ src/CSnakes.Tests/VersionParsingTests.cs
@@ -0,0 +1,13 @@
+using CSnakes.Runtime;
+
+namespace CSnakes.Tests;
+
+public class VersionParsingTests
+{
+    [Fact]
+    public void TestParsingVersions() {
+        Assert.Equal("3.12.3.0", ServiceCollectionExtensions.ParsePythonVersion("3.12.3").ToString());
+        Assert.Equal("3.10.3.0", ServiceCollectionExtensions.ParsePythonVersion("3.10.3").ToString());
+        Assert.Equal("3.14.0.0", ServiceCollectionExtensions.ParsePythonVersion("3.14.0-alpha.5").ToString());
+    }
+}

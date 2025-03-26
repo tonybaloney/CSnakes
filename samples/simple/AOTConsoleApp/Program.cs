19bc63e2ebb6909d88e197a158ef505d66a404dd
Anthony Shaw <anthony.p.shaw@gmail.com>
2025-03-26 17:23:02 +1100

    Add an AOT verification app

---
+++ samples/simple/AOTConsoleApp/Program.cs
@@ -0,0 +1,31 @@
+﻿using CSnakes.Runtime;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+var builder = Host.CreateDefaultBuilder(args)
+    .ConfigureServices(services =>
+    {
+        var home = Path.Join(Environment.CurrentDirectory, "..", "..", "..", "..", "ExamplePythonDependency");
+        var venv = Path.Join(home, ".venv");
+        services
+        .WithPython()
+        .WithHome(home)
+        .WithVirtualEnvironment(venv)
+        .FromNuGet("3.12.4")
+        .FromMacOSInstallerLocator("3.12")
+        .FromEnvironmentVariable("Python3_ROOT_DIR", "3.12")
+        .WithPipInstaller();
+    });
+
+var app = builder.Build();
+
+var env = app.Services.GetRequiredService<IPythonEnvironment>();
+
+RunQuickDemo(env);
+
+static void RunQuickDemo(IPythonEnvironment env)
+{
+    var quickDemo = env.QuickDemo();
+    Console.WriteLine(quickDemo.Scream("a", 99));
+    Console.WriteLine(string.Join(',', quickDemo.ScreamNames(["a", "b", "c"], 3)));
+}

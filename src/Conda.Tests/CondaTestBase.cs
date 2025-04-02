0801071b3092c6934581779497489ed8cd2bc31d
Konstantin Gukov <konstantin.gukov@microsoft.com>
2025-04-03 00:23:12 +0200

    More rebust search for conda source folder

--- src/Conda.Tests/CondaTestBase.cs
+++ src/Conda.Tests/CondaTestBase.cs
@@ -12,16 +12,26 @@ public class CondaTestBase : IDisposable
     {
         string condaEnv = Environment.GetEnvironmentVariable("CONDA") ?? string.Empty;
         string? pythonVersion = Environment.GetEnvironmentVariable("PYTHON_VERSION");
+
         if (string.IsNullOrEmpty(condaEnv))
         {
-            if (OperatingSystem.IsWindows())
-                condaEnv = Environment.GetEnvironmentVariable("LOCALAPPDATA") ?? "";
-            condaEnv = Path.Join(condaEnv, "anaconda3");
-            if (!Directory.Exists(condaEnv))
-            {
-                condaEnv = condaEnv.Replace("anaconda3", "miniconda3");
+            string? basePath = null;
+            if (OperatingSystem.IsWindows()) {
+                basePath = Environment.GetEnvironmentVariable("LOCALAPPDATA");
             }
+            else{
+                basePath = Environment.GetFolderPath(Environment.SpecialFolder.UserProfile);;
+            }
+
+            if (basePath is not null)
+            {
+                condaEnv = new[] { "anaconda3", "miniconda3" }
+                    .Select(condaName => Path.Join(basePath, condaName))
+                    .FirstOrDefault(Directory.Exists) ?? condaEnv;
+            }
+
         }
+
         var condaBinPath = OperatingSystem.IsWindows() ? Path.Join(condaEnv, "Scripts", "conda.exe") : Path.Join(condaEnv, "bin", "conda");
         var environmentSpecPath = Path.Join(Environment.CurrentDirectory, "python", "environment.yml");
         app = Host.CreateDefaultBuilder()

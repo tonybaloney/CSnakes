241ca2281b73217666fc2cd33b419950bbc8cf57
Atif Aziz <code@raboof.com>
2025-04-14 12:50:24 +0200

    Remove unused "PyLong_AsLong"

--- src/CSnakes.Runtime/CPython/Long.cs
+++ src/CSnakes.Runtime/CPython/Long.cs
@@ -31,24 +31,6 @@ internal unsafe partial class CPythonAPI
     [LibraryImport(PythonLibraryName, EntryPoint = "PyLong_AsLongLong")]
     private static partial long PyLong_AsLongLong_(PyObject p);
 
-    /// <summary>
-    /// Calls PyLong_AsLong and throws a Python Exception if an error occurs.
-    /// </summary>
-    /// <param name="p"></param>
-    /// <returns></returns>
-    internal static long PyLong_AsLong(PyObject p)
-    {
-        long result = PyLong_AsLong_(p);
-        if (result == -1 && PyErr_Occurred())
-        {
-            throw PyObject.ThrowPythonExceptionAsClrException("Error converting Python object to int, check that the object was a Python int or that the value wasn't too large. See InnerException for details.");
-        }
-        return result;
-    }
-
-    [LibraryImport(PythonLibraryName, EntryPoint = "PyLong_AsLong")]
-    private static partial int PyLong_AsLong_(PyObject p);
-
     internal static bool IsPyLong(PyObject p)
     {
         return PyObject_IsInstance(p, PyLongType);

b8ba08d0c7c99c7a546f6607b44dfd7c80b499b9
Brian Buvinghausen <buvinghausen@users.noreply.github.com>
2025-05-09 04:18:36 -0500

    Finish benchmarks

--- samples/simple/Benchmarks/Benchmarks.cs
+++ samples/simple/Benchmarks/Benchmarks.cs
@@ -50,13 +50,6 @@ public class InteropBenchmarks
         new(Services.NetGuidStr());
 
     /// <summary>
-    /// Benchmark for .NET Guid to Python uuid via string
-    /// </summary>
-    [Benchmark]
-    public void UuidAsStringGuidTest() =>
-        Services.PythonUuidStr(Guid.NewGuid().ToString());
-
-    /// <summary>
     /// Benchmark for .NET Guid to Python uuid letting .NET convert to big-endian
     /// </summary>
     [Benchmark]
@@ -70,21 +63,131 @@ public class InteropBenchmarks
     public void UuidAsLittleEndianGuidTest() =>
         Services.PythonUuidLittleEndian(Guid.NewGuid().ToByteArray());
 
+    /// <summary>
+    /// Benchmark for .NET Guid to Python uuid via string
+    /// </summary>
+    [Benchmark]
+    public void UuidAsStringGuidTest() =>
+        Services.PythonUuidStr(Guid.NewGuid().ToString());
+
+    /// <summary>
+    /// Benchmark for .NET DateOnly to Python date via string
+    /// </summary>
     [Benchmark]
     public void DateOnlyAsDateStringTest() =>
         Services.PythonDateStr($"{DateOnly.FromDateTime(DateTime.Now):O}");
 
+    /// <summary>
+    /// Benchmark for .NET DateOnly to Python date letting Python convert to DayNumber
+    /// </summary>
     [Benchmark]
     public void DateOnlyAsDateOrdinalTest() =>
         Services.PythonDateOrdinal(DateOnly.FromDateTime(DateTime.Now).DayNumber);
 
+    /// <summary>
+    /// Benchmark for Python date to .NET DateOnly via string
+    /// </summary>
     [Benchmark]
     public DateOnly DateAsDateOnlyStringTest() =>
         DateOnly.Parse(Services.NetDateStr(), CultureInfo.InvariantCulture);
 
+    /// <summary>
+    /// Benchmark for Python date to .NET DateOnly letting Python convert to DayNumber
+    /// </summary>
     [Benchmark]
     public DateOnly DateAsDateOnlyOrdinalTest() =>
         DateOnly.FromDayNumber((int)Services.NetDateOrdinal());
+
+    /// <summary>
+    /// Benchmark for Python time to .NET TimeOnly via string
+    /// </summary>
+    [Benchmark]
+    public TimeOnly TimeAsTimeOnlyStringTest() =>
+        TimeOnly.Parse(Services.NetTimeStr(), CultureInfo.InvariantCulture);
+
+    /// <summary>
+    /// Benchmark for Python time to .NET TimeOnly via microseconds
+    /// </summary>
+    [Benchmark]
+    public TimeOnly TimeAsTimeOnlyMicrosecondsTest() =>
+        TimeOnly.FromTimeSpan(TimeSpan.FromMicroseconds(Services.NetTimeMicroseconds()));
+
+    /// <summary>
+    /// Benchmark for .NET TimeOnly to Python time via string
+    /// </summary>
+    [Benchmark]
+    public void TimeOnlyAsTimeStringTest() =>
+        Services.PythonTimeStr($"{TimeOnly.FromDateTime(DateTime.Now):O}");
+
+    /// <summary>
+    /// Benchmark for .NET TimeOnly to Python time via microseconds
+    /// </summary>
+    [Benchmark]
+    public void TimeOnlyAsTimeMicrosecondsTest() =>
+        Services.PythonTimeMicroseconds(TimeOnly.FromDateTime(DateTime.Now).Ticks / TimeSpan.TicksPerMicrosecond);
+
+    /// <summary>
+    /// Benchmark for Python timedelta to .NET TimeSpan via string
+    /// </summary>
+    [Benchmark]
+    public TimeSpan TimeDeltaAsTimeSpanStringTest() =>
+        TimeSpan.Parse(Services.NetTimeDeltaStr(), CultureInfo.InvariantCulture);
+
+    /// <summary>
+    /// Benchmark for Python timedelta to .NET TimeSpan via microseconds
+    /// </summary>
+    [Benchmark]
+    public TimeSpan TimeDeltaAsTimeSpanMicrosecondsTest() =>
+        TimeSpan.FromMicroseconds(Services.NetTimeDeltaMicroseconds());
+
+    /// <summary>
+    /// Benchmark for .NET TimeSpan to Python timedelta via string
+    /// </summary>
+    [Benchmark]
+    public void TimeSpanAsTimeDeltaStringTest() =>
+        Services.PythonTimeDeltaStr($"{TimeSpan.FromSeconds(43018.125549):G}");
+
+    /// <summary>
+    /// Benchmark for .NET TimeSpan to Python timedelta via microseconds
+    /// </summary>
+    [Benchmark]
+    public void TimeSpanAsTimeDeltaMicrosecondsTest() =>
+        Services.PythonTimeMicroseconds(TimeSpan.FromSeconds(43018.125549).Ticks / TimeSpan.TicksPerMicrosecond);
+
+    /// <summary>
+    /// Benchmark for Python datetime to .NET DateTimeOffset via string
+    /// </summary>
+    [Benchmark]
+    public DateTimeOffset DateTimeAsDateTimeOffsetStringTest() =>
+        DateTimeOffset.Parse(Services.NetDateTimeStr(), CultureInfo.InvariantCulture);
+
+    /// <summary>
+    /// Benchmark for Python datetime to .NET DateTimeOffset via microseconds & offset seconds
+    /// </summary>
+    [Benchmark]
+    public DateTimeOffset DateTimeAsDateTimeOffsetMicrosecondsTest()
+    {
+        var (microseconds, offset) = Services.NetDateTimeOffset();
+        return new DateTimeOffset(new DateTime(microseconds * TimeSpan.TicksPerMicrosecond),
+            TimeSpan.FromSeconds(offset));
+    }
+
+    /// <summary>
+    /// Benchmark for .NET DateTimeOffset to Python datetime via string
+    /// </summary>
+    [Benchmark]
+    public void DateTimeOffsetAsDateTimeStringTest() =>
+        Services.PythonDateTimeStr($"{DateTimeOffset.Now:O}");
+
+    /// <summary>
+    /// Benchmark for .NET DateTimeOffset to Python datetime via microseconds & offset seconds
+    /// </summary>
+    [Benchmark]
+    public void DateTimeOffsetAsDateTimeMicrosecondsTest()
+    {
+        var dateTimeOffset = DateTimeOffset.Now;
+        Services.PythonDateTimeOffset((dateTimeOffset.Ticks / TimeSpan.TicksPerMicrosecond, (long)dateTimeOffset.Offset.TotalSeconds));
+    }
 
     [GlobalCleanup]
     public void GlobalCleanup()

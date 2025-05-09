31f140fa45bab435b45a65d53081b45170711b09
Brian Buvinghausen <buvinghausen@users.noreply.github.com>
2025-05-08 22:54:08 -0500

    Last pre-benchmark optimizations

--- samples/simple/ExamplePythonDependency/python/type_demos.py
+++ samples/simple/ExamplePythonDependency/python/type_demos.py
@@ -1,9 +1,9 @@
 from datetime import date, datetime, time, timedelta, timezone
 from uuid import uuid4, UUID
 
-SECONDS_PER_DAY = 86_400
-SECONDS_PER_HOUR = 3_600
 SECONDS_PER_MINUTE = 60
+SECONDS_PER_HOUR = 60 * SECONDS_PER_MINUTE
+SECONDS_PER_DAY = 24 * SECONDS_PER_HOUR
 MICROSECONDS_PER_SECOND = 1_000_000
 
 sample_dict = {'a': 1, 'b': 2, 'c': 3}
@@ -15,7 +15,7 @@ def take_dict(d: dict[str, int]) -> bool:
     print(d)
     return d == sample_dict
 
-#Note: Python uuid is big-endian and .NET is little-endian
+#Note: Python uuid is big-endian and .NET Guid is little-endian
 def return_uuid() -> tuple[bytes, bytes, str]:
     val = uuid4()
     return val.bytes, val.bytes_le, str(val)
@@ -42,7 +42,7 @@ def take_time(microseconds: int, iso_string: str) -> bool:
 
 # Python time is in microseconds so interop at that level
 def return_time_delta() -> tuple[int, str]:
-    val = timedelta(microseconds= 101_234_543_018_125_549) # Use gigantic value
+    val = timedelta(microseconds=101_234_543_018_125_549) # Use gigantic value
     hours, remainder = divmod(val.seconds, SECONDS_PER_HOUR)
     minutes, seconds = divmod(remainder, SECONDS_PER_MINUTE)
     return (val.days * SECONDS_PER_DAY + val.seconds) * MICROSECONDS_PER_SECOND + val.microseconds, f'{val.days}:{str(hours).zfill(2)}:{str(minutes).zfill(2)}:{str(seconds).zfill(2)}.{str(val.microseconds).zfill(6)}'
@@ -61,10 +61,10 @@ def return_date_time(use_utc: bool = True) -> tuple[tuple[int, int], str]:
         offset = val.astimezone().utcoffset()
     return (((val.toordinal() - 1) * SECONDS_PER_DAY + val.hour * SECONDS_PER_HOUR + val.minute * SECONDS_PER_MINUTE + val.second) * MICROSECONDS_PER_SECOND + val.microsecond, int(offset.total_seconds())), val.isoformat()
 
-def roundtrip_date_time_iso(iso_datetime: str) -> str:
-    return datetime.fromisoformat(iso_datetime).isoformat()
-
 def take_date_time_offset(offset_tuple: tuple[int, int], iso_string: str) -> bool:
     days, microseconds = divmod(offset_tuple[0], SECONDS_PER_DAY * MICROSECONDS_PER_SECOND)
     val = datetime.fromordinal(days + 1) + timedelta(microseconds=microseconds)
     return datetime(val.year, val.month, val.day, val.hour, val.minute, val.second, val.microsecond, timezone(timedelta(seconds=offset_tuple[1]))) == datetime.fromisoformat(iso_string)
+
+def roundtrip_date_time_iso(iso_datetime: str) -> str:
+    return datetime.fromisoformat(iso_datetime).isoformat()

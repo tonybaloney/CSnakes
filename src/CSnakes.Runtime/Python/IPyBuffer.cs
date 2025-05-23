fb4fe470fa3f3b49d0f48a3fa7ebb438cd9415c9
Atif Aziz <code@raboof.com>
2025-05-23 19:31:24 +0200

    Fix leaks by making "IPyBuffer" disposable

--- src/CSnakes.Runtime/Python/IPyBuffer.cs
+++ src/CSnakes.Runtime/Python/IPyBuffer.cs
@@ -4,7 +4,7 @@ using System.Numerics.Tensors;
 #endif
 
 namespace CSnakes.Runtime.Python;
-public interface IPyBuffer
+public interface IPyBuffer : IDisposable
 {
     /// <summary>
     /// The bit length of the buffer.

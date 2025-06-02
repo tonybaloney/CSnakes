8f446ad95efd0b427193a5d6c6e4424955026b42
Anthony Shaw <anthony.p.shaw@gmail.com>
2025-06-02 16:36:53 +1000

    Fix some of the typing issues

--- src/Integration.Tests/python/test_false_returns.py
+++ src/Integration.Tests/python/test_false_returns.py
@@ -1,35 +1,35 @@
 def test_str_actually_returns_int() -> str:
-    return 1
+    return 1  # type: ignore[return-value]
 
 def test_str_actually_returns_float() -> str:
-    return 1.0
+    return 1.0  # type: ignore[return-value]
 
 def test_str_actually_returns_list() -> str:
-    return [1, 2, 3]
+    return [1, 2, 3]  # type: ignore[return-value]
 
 def test_str_actually_returns_tuple() -> str:
-    return (1, 2, 3)
+    return (1, 2, 3)  # type: ignore[return-value]
 
 def test_tuple_actually_returns_int() -> tuple[int, int]:
-    return 1
+    return 1  # type: ignore[return-value]
 
 def test_tuple_actually_returns_float() -> tuple[float, float]:
-    return 1.0
+    return 1.0  # type: ignore[return-value]
 
 def test_tuple_actually_returns_list() -> tuple[list[int], list[int]]:
-    return [1, 2, 3]
+    return [1, 2, 3]  # type: ignore[return-value]
 
 def test_float_returns_int() -> float:
     return 1
 
 def test_float_returns_str() -> float:
-    return "1"
+    return "1"  # type: ignore[return-value]
 
 def test_int_returns_float() -> int:
-    return 1.0
+    return 1.0  # type: ignore[return-value]
 
 def test_int_returns_str() -> int:
-    return "1"
+    return "1"  # type: ignore[return-value]
 
 def test_int_overflows() -> int:
     return 2**64

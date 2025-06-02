091be8c5da567accf4fb389743cda3d0a1929419
Anthony Shaw <anthony.p.shaw@gmail.com>
2025-06-02 16:27:17 +1000

    Wire up iterables

--- src/Integration.Tests/IterableTests.cs
+++ src/Integration.Tests/IterableTests.cs
@@ -1,13 +1,15 @@
-using CSnakes.Runtime.Python;
-
 namespace Integration.Tests;
+
 public class IterableTests(PythonEnvironmentFixture fixture) : IntegrationTestBase(fixture)
 {
     [Fact]
     public void TestBuiltinIterable()
     {
         var mod = Env.TestIterable();
-        using PyObject result = mod.TestBuiltinIterable();
-        Assert.True(result.IsNone());
+        using var result = mod.TestBuiltinIterable();
+        foreach (var item in result)
+        {
+            Assert.Equal("item", item);
+        }
     }
 }

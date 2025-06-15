50af296369b59cecbe4e34c58cb71ee8deb26c91
Anthony Shaw <anthony.p.shaw@gmail.com>
2025-06-15 16:52:44 +1000

    Fix nullability of default value types

--- src/CSnakes.Tests/GeneratedSignatureTests.cs
+++ src/CSnakes.Tests/GeneratedSignatureTests.cs
@@ -47,7 +47,7 @@ public class GeneratedSignatureTests
     [InlineData("def hello() -> Generator[int, str, bool]:\n ...\n", "IGeneratorIterator<long, string, bool> Hello()")]
     [InlineData("def hello() -> typing.Generator[int, str, bool]:\n ...\n", "IGeneratorIterator<long, string, bool> Hello()")]
     [InlineData("def hello() -> Buffer:\n ...\n", "IPyBuffer Hello()")]
-    [InlineData("def hello(a: Union[int, str] = 5) -> Any:\n ...\n", "PyObject Hello(PyObject a = null)")]
+    [InlineData("def hello(a: Union[int, str] = 5) -> Any:\n ...\n", "PyObject Hello(PyObject? a = null)")]
     [InlineData("def hello(data: Literal[1, 'two', 3.0]) -> None:\n ...\n", "void Hello(PyObject data)")]
     public void TestGeneratedSignature(string code, string expected)
     {

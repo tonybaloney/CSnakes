8bbb735811168406d2ddf6545949e0ac2aca4b4e
Anthony Shaw <anthony.p.shaw@gmail.com>
2025-06-19 12:07:41 +1000

    Use a generator

--- src/Profile/python/numpy_benchmarks.py
+++ src/Profile/python/numpy_benchmarks.py
@@ -2,18 +2,19 @@ import numpy as np
 from ml_dtypes import bfloat16
 from typing_extensions import Buffer
 
+rng = np.random.default_rng(seed=1337)
 
 def generate_2d_float32_array(rows: int, cols: int) -> Buffer:
-    return np.random.rand(rows, cols).astype(np.float32)
+    return rng.random((rows, cols)).astype(np.float32)
 
 def generate_2d_float32_array_as_bytes(rows: int, cols: int) -> Buffer:
-    return np.random.rand(rows, cols).astype(np.float32).tobytes()
+    return rng.random((rows, cols)).astype(np.float32).tobytes()
 
 def generate_2d_float64_array(rows: int, cols: int) -> Buffer:
-    return np.random.rand(rows, cols).astype(np.float64)
+    return rng.random((rows, cols)).astype(np.float64)
 
 def generate_2d_float64_array_as_bytes(rows: int, cols: int) -> Buffer:
-    return np.random.rand(rows, cols).astype(np.float64).tobytes()
+    return rng.random((rows, cols)).astype(np.float64).tobytes()
 
 def generate_2d_bfloat16_array_as_bytes(rows: int, cols: int) -> Buffer:
-    return np.random.rand(rows, cols).astype(bfloat16).tobytes()
+    return rng.random((rows, cols)).astype(bfloat16).tobytes()

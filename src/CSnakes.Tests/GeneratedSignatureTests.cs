ebb4cc86acdf14778103b3d3e8b38931247301b5
Atif Aziz <code@raboof.com>
2025-06-25 07:17:27 +0200

    Add test for case when "bytes" is defaulted

--- src/CSnakes.Tests/GeneratedSignatureTests.cs
+++ src/CSnakes.Tests/GeneratedSignatureTests.cs
@@ -32,6 +32,7 @@ public class GeneratedSignatureTests
     [InlineData("def hello_world(a: int | None, b: str | None) -> bool | None: \n ...\n", "bool? HelloWorld(long? a, string? b)")]
     [InlineData("def hello_world(a: bytes, b: bool = False, c: float = 0.1) -> None: \n ...\n", "void HelloWorld(ReadOnlySpan<byte> a, bool b = false, double c = 0.1)")]
     [InlineData("def hello_world(a: bytes) -> bytes: \n ...\n", "byte[] HelloWorld(ReadOnlySpan<byte> a)")]
+    [InlineData("def hello_world(a: bytes = b'hello') -> bytes: \n ...\n", "byte[] HelloWorld(byte[]? a = null)")]
     [InlineData("async def hello_world(a: bytes, b: bool = False, c: float = 0.1) -> None: \n ...\n", "Task HelloWorld(ReadOnlySpan<byte> a, bool b = false, double c = 0.1, CancellationToken cancellationToken = default)")]
     [InlineData("async def hello_world(a: bytes) -> bytes: \n ...\n", "Task<byte[]> HelloWorld(ReadOnlySpan<byte> a, CancellationToken cancellationToken = default)")]
     [InlineData("def hello_world(a: str = 'default') -> None: \n ...\n", "void HelloWorld(string a = \"default\")")]

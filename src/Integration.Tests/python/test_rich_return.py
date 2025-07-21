e0283bb72025830e08d8e90eb4f7d989adaa5166
Atif Aziz <code@raboof.com>
2025-07-21 08:55:20 +0200

    Ignore "reportUndefinedVariable" warning for our forward ref

--- src/Integration.Tests/python/test_rich_return.py
+++ src/Integration.Tests/python/test_rich_return.py
@@ -9,11 +9,11 @@ class FooBarBaz:
     qux: tuple[int, str]
     quux: dict[str, int]
 
-def foo_bar_baz() -> "__extern__.Integration.Tests.RichReturnTests.FooBarBaz":
+def foo_bar_baz() -> "__extern__.Integration.Tests.RichReturnTests.FooBarBaz": # pyright: ignore[reportUndefinedVariable]
     return FooBarBaz(1, "hello", [1, 2, 3], (42, "world"), { "foo": 1, "bar": 2, "baz": 3 })
 
-def foo_bar_baz_list() -> list["__extern__.Integration.Tests.RichReturnTests.FooBarBaz"]:
+def foo_bar_baz_list() -> list["__extern__.Integration.Tests.RichReturnTests.FooBarBaz"]: # pyright: ignore[reportUndefinedVariable]
     return [foo_bar_baz()]
 
-def foo_bar_baz_dict(key: str) -> dict[str, "__extern__.Integration.Tests.RichReturnTests.FooBarBaz"]:
+def foo_bar_baz_dict(key: str) -> dict[str, "__extern__.Integration.Tests.RichReturnTests.FooBarBaz"]: # pyright: ignore[reportUndefinedVariable]
     return { key: foo_bar_baz() }

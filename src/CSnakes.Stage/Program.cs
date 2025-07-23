ccf9c20153f2124039cef949dfe15489a5a378e8
Anthony Shaw <anthony.p.shaw@gmail.com>
2025-07-23 14:26:04 +1000

    Add download switch

--- src/CSnakes.Stage/Program.cs
+++ src/CSnakes.Stage/Program.cs
@@ -1,4 +1,9 @@
-﻿using System.Reflection;
+using CSnakes.Runtime;
+using CSnakes.Runtime.Locators;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System.CommandLine;
+using System.Reflection;
 
 namespace CSnakes.Stage;
 
@@ -6,19 +11,44 @@ internal class Program
 {
     static void Main(string[] args)
     {
-        if (args.Length == 0)
-        {
-            var versionString = Assembly.GetEntryAssembly()?
+        var versionString = Assembly.GetEntryAssembly()?
                                     .GetCustomAttribute<AssemblyInformationalVersionAttribute>()?
                                     .InformationalVersion
                                     .ToString();
 
-            Console.WriteLine($"CSnakes.Stage v{versionString}");
-            Console.WriteLine("-------------");
-            Console.WriteLine("\nUsage:");
-            Console.WriteLine("  CSnakes.Stage --version 3.12");
-            return;
-        }
+        Option<RedistributablePythonVersion> versionOption = new("--version", "The Python version to use (e.g., 3.12)")
+        {
+            Required = true
+        };
+        var rootCommand = new RootCommand
+        {
+            versionOption
+        };
 
+        rootCommand.Description = $"CSnakes.Stage v{versionString} - A tool to manage Python environments and versions.";
+
+        rootCommand.SetAction((version) =>
+        {
+            Stage(version.GetRequiredValue(versionOption));
+        });
+    }
+
+    private static void Stage(RedistributablePythonVersion version = RedistributablePythonVersion.Python3_12)
+    {
+        Console.WriteLine($"Staging CSnakes for Python {version}...");
+
+        var builder = Host.CreateApplicationBuilder();
+        var home = Path.Join(Environment.CurrentDirectory);
+        builder.Services
+            .WithPython()
+            .WithHome(home)
+            .FromRedistributable(version);
+
+        var app = builder.Build();
+
+        var locator = app.Services.GetRequiredService<PythonLocator>();
+        var location = locator.LocatePython();
+        
+        Console.WriteLine($"Python {version} downloaded and located at: {location.PythonPath}");    
     }
 }

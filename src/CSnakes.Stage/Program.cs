5a3f7a89b07fbff46703700752cd7c1556463080
Anthony Shaw <anthony.p.shaw@gmail.com>
2025-07-23 13:58:27 +1000

    Add staging project setup

---
+++ src/CSnakes.Stage/Program.cs
@@ -0,0 +1,24 @@
+﻿using System.Reflection;
+
+namespace CSnakes.Stage;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        if (args.Length == 0)
+        {
+            var versionString = Assembly.GetEntryAssembly()?
+                                    .GetCustomAttribute<AssemblyInformationalVersionAttribute>()?
+                                    .InformationalVersion
+                                    .ToString();
+
+            Console.WriteLine($"CSnakes.Stage v{versionString}");
+            Console.WriteLine("-------------");
+            Console.WriteLine("\nUsage:");
+            Console.WriteLine("  CSnakes.Stage --version 3.12");
+            return;
+        }
+
+    }
+}

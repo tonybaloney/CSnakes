4e838cffdeac30cc0fea52ea7b4049cba67399a9
Anthony Shaw <anthony.p.shaw@gmail.com>
2025-08-13 16:27:13 +1000

    Run test suite with redistributable

--- src/Integration.Tests/IntegrationTestBase.cs
+++ src/Integration.Tests/IntegrationTestBase.cs
@@ -1,9 +1,10 @@
-using System;
-using System.IO;
+using CSnakes.Runtime.Locators;
 using CSnakes.Runtime.PackageManagement;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using System;
+using System.IO;
 
 namespace Integration.Tests;
 
@@ -23,20 +24,27 @@ public sealed class PythonEnvironmentFixture : IDisposable
 
     public PythonEnvironmentFixture()
     {
-        string pythonVersionWindows = Environment.GetEnvironmentVariable("PYTHON_VERSION") ?? "3.12.9";
-        string pythonVersionMacOS = Environment.GetEnvironmentVariable("PYTHON_VERSION") ?? "3.12";
-        string pythonVersionLinux = Environment.GetEnvironmentVariable("PYTHON_VERSION") ?? "3.12";
-        bool freeThreaded = Environment.GetEnvironmentVariable("PYTHON_FREETHREADED") == "true";
-        string venvPath = Path.Join(Environment.CurrentDirectory, "python", ".venv");
+        Version pythonVersionToTest = ServiceCollectionExtensions.ParsePythonVersion(Environment.GetEnvironmentVariable("PYTHON_VERSION") ?? "3.12.9");
+        bool freeThreaded = Environment.GetEnvironmentVariable("PYTHON_FREETHREADED") == "1";
+        bool debugPython = Environment.GetEnvironmentVariable("PYTHON_DEBUG") == "1";
+        string venvPath = Path.Join(Environment.CurrentDirectory, "python", $".venv-{pythonVersionToTest}");
+
+        RedistributablePythonVersion redistributableVersion = pythonVersionToTest.Minor switch
+        {
+            9 => RedistributablePythonVersion.Python3_9,
+            10 => RedistributablePythonVersion.Python3_10,
+            11 => RedistributablePythonVersion.Python3_11,
+            12 => RedistributablePythonVersion.Python3_12,
+            13 => RedistributablePythonVersion.Python3_13,
+            14 => RedistributablePythonVersion.Python3_14,
+            _ => throw new NotSupportedException($"Python version {pythonVersionToTest} is not supported.")
+        };
 
         var builder = Host.CreateApplicationBuilder();
         var pb = builder.Services.WithPython();
         pb.WithHome(Path.Join(Environment.CurrentDirectory, "python"));
 
-        pb.FromNuGet(pythonVersionWindows)
-          .FromMacOSInstallerLocator(pythonVersionMacOS, freeThreaded)
-          .FromWindowsStore("3.12")
-          .FromEnvironmentVariable("Python3_ROOT_DIR", pythonVersionLinux)
+        pb.FromRedistributable(version: redistributableVersion, debug: debugPython, freeThreaded: freeThreaded)
           .WithVirtualEnvironment(venvPath)
           .WithPipInstaller();
 

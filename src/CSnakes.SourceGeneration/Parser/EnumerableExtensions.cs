b868018ac0d11e17ca6b1026a8a621c1ea9f4148
Atif Aziz <code@raboof.com>
2025-08-18 17:34:18 +0200

    Remove extraneous space

--- src/CSnakes.SourceGeneration/Parser/EnumerableExtensions.cs
+++ src/CSnakes.SourceGeneration/Parser/EnumerableExtensions.cs
@@ -69,5 +69,5 @@ internal static partial class EnumerableExtensions
     }
 
     public static IEnumerable<T> Append<T>(this IEnumerable<T> source, T item) =>
-        source.Concat([ item ]);
+        source.Concat([item]);
 }

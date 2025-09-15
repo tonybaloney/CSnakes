88898a8ef90b0e9809c7e1ed01cccfa362e1c4f0
Anthony Shaw <anthony.p.shaw@gmail.com>
2025-09-15 19:32:10 +1000

    Tidy up some unused parameters

--- src/CSnakes.Runtime/PythonLogger.cs
+++ src/CSnakes.Runtime/PythonLogger.cs
@@ -7,7 +7,6 @@ namespace CSnakes.Runtime;
 /// Bridges .NET logging to Python logging.
 /// Create a Python log handler that stores logs in memory and emits them back to .NET.
 /// </summary>
-/// <param name="logger"></param>
 public static class PythonLogger
 {
     // TODO : Handle Close request via a destructor or IDisposable pattern.
@@ -15,14 +14,12 @@ public static class PythonLogger
 
     public static void WithPythonLogging(this IPythonEnvironment env, ILogger logger)
     {
-        using var handler = CreateHandler(env);
+        using var handler = CreateHandler();
         RecordListener(handler, logger);
     }
 
-    internal static PyObject CreateHandler(IPythonEnvironment env, string? logger = null)
+    internal static PyObject CreateHandler(string? loggerName = null)
     {
-        using PyObject loggingHandlersModule = Import.ImportModule("logging.handlers");
-        using PyObject memoryHandlerClass = loggingHandlersModule.GetAttr("MemoryHandler");
         string handlerPythonCode = @"
 import logging
 import time
@@ -52,12 +49,16 @@ def installCSnakesHandler(handler, name = None):
     logging.getLogger(name).addHandler(handler)
 
 ";
-        using var module = Import.ImportModule("_csnakesLoggingBridge", handlerPythonCode, "_csnakesLoggingBridge.py");
-        using var __csnakesMemoryHandlerCls = module.GetAttr("__csnakesMemoryHandler");
-        var handler = __csnakesMemoryHandlerCls.Call();
-        using var __installCSnakesHandler = module.GetAttr("installCSnakesHandler");
-        __installCSnakesHandler.Call(handler, PyObject.From(logger));
-        return handler;
+        using (GIL.Acquire())
+        {
+            using var module = Import.ImportModule("_csnakesLoggingBridge", handlerPythonCode, "_csnakesLoggingBridge.py");
+            using var __csnakesMemoryHandlerCls = module.GetAttr("__csnakesMemoryHandler");
+            var handler = __csnakesMemoryHandlerCls.Call();
+            using var __installCSnakesHandler = module.GetAttr("installCSnakesHandler");
+            __installCSnakesHandler.Call(handler, PyObject.From(loggerName));
+
+            return handler;
+        }
     }
 
     private static void HandleRecord(ILogger logger, int level, string message)

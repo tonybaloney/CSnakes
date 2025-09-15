564c8e5f111fd142f4525ce7efa69cd1d1e60b95
Atif Aziz <code@raboof.com>
2025-09-15 14:05:20 +0200

    Cache type def sub-parsers

--- src/CSnakes.SourceGeneration/Parser/PythonParser.TypeDef.cs
+++ src/CSnakes.SourceGeneration/Parser/PythonParser.TypeDef.cs
@@ -2,6 +2,7 @@ using CSnakes.Parser.Types;
 using Superpower;
 using Superpower.Model;
 using Superpower.Parsers;
+using PythonTypeSpecParser = Superpower.TokenListParser<CSnakes.Parser.PythonToken, CSnakes.Parser.Types.PythonTypeSpec>;
 
 namespace CSnakes.Parser;
 public static partial class PythonParser
@@ -21,85 +22,108 @@ public static partial class PythonParser
 
     public static TokenListParser<PythonToken, PythonTypeSpec> PythonTypeDefinitionParser { get; }
 
+    private static class TypeDefinitionSubParsers
+    {
+        private static readonly TokenListParser<PythonToken, Token<PythonToken>> Comma = Token.EqualTo(PythonToken.Comma);
+
+        public static readonly PythonTypeSpecParser None = Return((PythonTypeSpec)PythonTypeSpec.None);
+        public static readonly PythonTypeSpecParser Any = Return((PythonTypeSpec)PythonTypeSpec.Any);
+        public static readonly PythonTypeSpecParser Int = Return((PythonTypeSpec)PythonTypeSpec.Int);
+        public static readonly PythonTypeSpecParser Str = Return((PythonTypeSpec)PythonTypeSpec.Str);
+        public static readonly PythonTypeSpecParser Float = Return((PythonTypeSpec)PythonTypeSpec.Float);
+        public static readonly PythonTypeSpecParser Bool = Return((PythonTypeSpec)PythonTypeSpec.Bool);
+        public static readonly PythonTypeSpecParser Bytes = Return((PythonTypeSpec)PythonTypeSpec.Bytes);
+        public static readonly PythonTypeSpecParser Buffer = Return((PythonTypeSpec)PythonTypeSpec.Buffer);
+
+        private static readonly PythonTypeSpecParser TypeDefinitionParser = Parse.Ref(() => PythonTypeDefinitionParser);
+
+        public static readonly PythonTypeSpecParser Optional = TypeDefinitionParser.Subscript(PythonTypeSpec (of) => new OptionalType(of));
+        public static readonly PythonTypeSpecParser List = TypeDefinitionParser.Subscript(PythonTypeSpec (of) => new ListType(of));
+        public static readonly PythonTypeSpecParser Sequence = TypeDefinitionParser.Subscript(PythonTypeSpec (of) => new SequenceType(of));
+        public static readonly PythonTypeSpecParser Dict = TypeDefinitionParser.Subscript(PythonTypeSpec (k, v) => new DictType(k, v));
+        public static readonly PythonTypeSpecParser Mapping = TypeDefinitionParser.Subscript(PythonTypeSpec (k, v) => new MappingType(k, v));
+        public static readonly PythonTypeSpecParser Generator = TypeDefinitionParser.Subscript(PythonTypeSpec (y, s, r) => new GeneratorType(y, s, r));
+        public static readonly PythonTypeSpecParser Coroutine = TypeDefinitionParser.Subscript(PythonTypeSpec (y, s, r) => new CoroutineType(y, s, r));
+
+        public static readonly PythonTypeSpecParser Callable =
+            // > The subscription syntax must always be used with exactly two values: the argument list and the
+            // > return type. The argument list must be a list of types, a `ParamSpec`, `Concatenate`, or an
+            // > ellipsis. The return type must be a single type.
+            //
+            // Source: Annotating callable objects,
+            // <https://docs.python.org/3/library/typing.html#annotating-callable-objects>
+            from callable in
+                TypeDefinitionParser.ManyDelimitedBy(Comma)
+                                    .Subscript()
+                                    .ThenIgnore(Comma)
+                                    .Then(ps => from r in TypeDefinitionParser
+                                                select (Parameters: ps, Return: r))
+                                    .Subscript()
+            select (PythonTypeSpec)new CallbackType([..callable.Parameters], callable.Return);
+
+        public static readonly PythonTypeSpecParser Literal =
+            // Literal can contain any PythonConstant, or a list of them.
+            // See PEP586 https://peps.python.org/pep-0586/
+            // It can also contain an expression, like a typedef but we don't support that yet.
+            // It could also contain another Literal, but we don't support that yet either.
+            from literals in
+                ConstantValueParser.AtLeastOnceDelimitedBy(Comma)
+                    .Subscript()
+            select (PythonTypeSpec)new LiteralType([..literals]);
+
+        public static readonly PythonTypeSpecParser Tuple =
+            Parse.OneOf(from of in TypeDefinitionParser.ThenIgnore(Comma)
+                                                       .ThenIgnore(Token.EqualTo(PythonToken.Ellipsis))
+                                                       .Subscript()
+                                                       .Try()
+                        select (PythonTypeSpec)new VariadicTupleType(of),
+                        from subscript in TypeDefinitionParser.AtLeastOnceDelimitedBy(Comma)
+                                                              .Subscript()
+                        select (PythonTypeSpec)new TupleType([..subscript]))
+                 .OptionalOrDefault(PythonTypeSpec.Tuple);
+
+        public static readonly PythonTypeSpecParser Union =
+            from subscript in
+                TypeDefinitionParser.AtLeastOnceDelimitedBy(Comma)
+                                    .Subscript()
+            select UnionType.Normalize(subscript);
+
+        public static readonly TokenListParser<PythonToken, PythonTypeSpec[]> Subscript =
+            TypeDefinitionParser.AtLeastOnceDelimitedBy(Comma)
+                                .Subscript()
+                                .OptionalOrDefault([]);
+    }
+
     private static TokenListParser<PythonToken, PythonTypeSpec> CreatePythonTypeDefinitionParser()
     {
-        var typeDefinitionParser = Parse.Ref(() => PythonTypeDefinitionParser);
-
-        var comma = Token.EqualTo(PythonToken.Comma);
-
         var parser =
             from name in Parse.OneOf(Token.EqualTo(PythonToken.None),
                                      Token.EqualTo(PythonToken.Identifier),
                                      Token.EqualTo(PythonToken.QualifiedIdentifier))
             from type in name.ToStringValue() switch
             {
-                "None" => Return((PythonTypeSpec)PythonTypeSpec.None),
-                "Any" => Return((PythonTypeSpec)PythonTypeSpec.Any),
-                "int" => Return((PythonTypeSpec)PythonTypeSpec.Int),
-                "str" => Return((PythonTypeSpec)PythonTypeSpec.Str),
-                "float" => Return((PythonTypeSpec)PythonTypeSpec.Float),
-                "bool" => Return((PythonTypeSpec)PythonTypeSpec.Bool),
-                "bytes" => Return((PythonTypeSpec)PythonTypeSpec.Bytes),
-                "Buffer" or "collections.abc.Buffer" => Return((PythonTypeSpec)PythonTypeSpec.Buffer),
-                "Optional" or "typing.Optional" =>
-                    typeDefinitionParser.Subscript(PythonTypeSpec (of) => new OptionalType(of)),
-                "list" or "List" or "typing.List" =>
-                    typeDefinitionParser.Subscript(PythonTypeSpec (of) => new ListType(of)),
-                "Sequence" or "collections.abc.Sequence" or "typing.Sequence" =>
-                    typeDefinitionParser.Subscript(PythonTypeSpec (of) => new SequenceType(of)),
-                "dict" or "Dict" or "typing.Dict" =>
-                    typeDefinitionParser.Subscript(PythonTypeSpec (k, v) => new DictType(k, v)),
-                "Mapping" or "collections.abc.Mapping" or "typing.Mapping" =>
-                    typeDefinitionParser.Subscript(PythonTypeSpec (k, v) => new MappingType(k, v)),
-                "Generator" or "collections.abc.Generator" or "typing.Generator" =>
-                    typeDefinitionParser.Subscript(PythonTypeSpec (y, s, r) => new GeneratorType(y, s, r)),
-                "Coroutine" or "collections.abc.Coroutine" or "typing.Coroutine" =>
-                    typeDefinitionParser.Subscript(PythonTypeSpec (y, s, r) => new CoroutineType(y, s, r)),
-                "Callable" or "typing.Callable" or "collections.abc.Callable" =>
-                    // > The subscription syntax must always be used with exactly two values: the argument list and the
-                    // > return type. The argument list must be a list of types, a `ParamSpec`, `Concatenate`, or an
-                    // > ellipsis. The return type must be a single type.
-                    //
-                    // Source: Annotating callable objects,
-                    // <https://docs.python.org/3/library/typing.html#annotating-callable-objects>
-                    from callable in
-                        typeDefinitionParser.ManyDelimitedBy(comma)
-                                            .Subscript()
-                                            .ThenIgnore(comma)
-                                            .Then(ps => from r in typeDefinitionParser
-                                                        select (Parameters: ps, Return: r))
-                                            .Subscript()
-                    select (PythonTypeSpec)new CallbackType([..callable.Parameters], callable.Return),
-                "Literal" or "typing.Literal" =>
-                    // Literal can contain any PythonConstant, or a list of them.
-                    // See PEP586 https://peps.python.org/pep-0586/
-                    // It can also contain an expression, like a typedef but we don't support that yet.
-                    // It could also contain another Literal, but we don't support that yet either.
-                    from literals in
-                        ConstantValueParser.AtLeastOnceDelimitedBy(comma)
-                                           .Subscript()
-                    select (PythonTypeSpec)new LiteralType([..literals]),
-                "Union" or "typing.Union" =>
-                    from subscript in
-                        typeDefinitionParser.AtLeastOnceDelimitedBy(comma)
-                                            .Subscript()
-                    select UnionType.Normalize(subscript),
-                "tuple" or "Tuple" or "typing.Tuple" =>
-                    Parse.OneOf(from of in typeDefinitionParser.ThenIgnore(comma)
-                                                               .ThenIgnore(Token.EqualTo(PythonToken.Ellipsis))
-                                                               .Subscript()
-                                                               .Try()
-                                select (PythonTypeSpec)new VariadicTupleType(of),
-                                from subscript in typeDefinitionParser.AtLeastOnceDelimitedBy(comma)
-                                                                      .Subscript()
-                                select (PythonTypeSpec)new TupleType([..subscript]))
-                         .OptionalOrDefault(PythonTypeSpec.Tuple),
-                var other =>
-                    from subscript in
-                        typeDefinitionParser.AtLeastOnceDelimitedBy(comma)
-                                            .Subscript()
-                                            .OptionalOrDefault([])
-                    select (PythonTypeSpec)new ParsedPythonTypeSpec(other, [..subscript]),
+                "None"                                                           => TypeDefinitionSubParsers.None,
+                "Any"                                                            => TypeDefinitionSubParsers.Any,
+                "int"                                                            => TypeDefinitionSubParsers.Int,
+                "str"                                                            => TypeDefinitionSubParsers.Str,
+                "float"                                                          => TypeDefinitionSubParsers.Float,
+                "bool"                                                           => TypeDefinitionSubParsers.Bool,
+                "bytes"                                                          => TypeDefinitionSubParsers.Bytes,
+                "Buffer" or "collections.abc.Buffer"                             => TypeDefinitionSubParsers.Buffer,
+                "Optional" or "typing.Optional"                                  => TypeDefinitionSubParsers.Optional,
+                "list" or "List" or "typing.List"                                => TypeDefinitionSubParsers.List,
+                "Sequence" or "collections.abc.Sequence" or "typing.Sequence"    => TypeDefinitionSubParsers.Sequence,
+                "dict" or "Dict" or "typing.Dict"                                => TypeDefinitionSubParsers.Dict,
+                "Mapping" or "collections.abc.Mapping" or "typing.Mapping"       => TypeDefinitionSubParsers.Mapping,
+                "Generator" or "collections.abc.Generator" or "typing.Generator" => TypeDefinitionSubParsers.Generator,
+                "Coroutine" or "collections.abc.Coroutine" or "typing.Coroutine" => TypeDefinitionSubParsers.Coroutine,
+                "Callable" or "typing.Callable" or "collections.abc.Callable"    => TypeDefinitionSubParsers.Callable,
+                "Literal" or "typing.Literal"                                    => TypeDefinitionSubParsers.Literal,
+                "Union" or "typing.Union"                                        => TypeDefinitionSubParsers.Union,
+                "tuple" or "Tuple" or "typing.Tuple"                             => TypeDefinitionSubParsers.Tuple,
+
+                var other => from subscript in TypeDefinitionSubParsers.Subscript
+                             select (PythonTypeSpec)new ParsedPythonTypeSpec(other, [..subscript]),
             }
             select type;
 

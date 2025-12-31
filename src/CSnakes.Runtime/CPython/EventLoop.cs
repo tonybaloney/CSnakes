97e85e02333cd09b2fb09a316d1242f616480844
Bastian Burger <bastian.burger@microsoft.com>
2025-12-31 14:30:53 +0100

    Revert "Use HashSet for more efficient cancellation"
    This reverts commit c2cd462a5f7c5ec0c94978706695bcb295778812.

--- src/CSnakes.Runtime/CPython/EventLoop.cs
+++ src/CSnakes.Runtime/CPython/EventLoop.cs
@@ -216,7 +216,7 @@ internal sealed class EventLoop : IDisposable
     private void RunForever()
     {
         var state = RunState.Running;
-        var coroTasks = new HashSet<CoroutineTask>();
+        var coroTasks = new List<CoroutineTask>();
 
         do
         {
@@ -307,8 +307,7 @@ internal sealed class EventLoop : IDisposable
                         }
                         case (CancelRequest request, _):
                         {
-                            if (coroTasks.TryGetValue(request.Task, out var task))
-                                task.Cancel(request.CancellationToken);
+                            coroTasks.Find(t => ReferenceEquals(t, request.Task))?.Cancel(request.CancellationToken);
                             break;
                         }
                         case (StopRequest, RunState.Running):
@@ -322,7 +321,7 @@ internal sealed class EventLoop : IDisposable
                 }
             }
 
-            _ = coroTasks.RemoveWhere(t => t.Conclude());
+            _ = coroTasks.RemoveAll(t => t.Conclude());
         }
         while (state is RunState.Running || coroTasks.Count > 0);
     }

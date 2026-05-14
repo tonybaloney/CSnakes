a828e73a11adb14ff0ed0f05682ae40428f4c4d4
Atif Aziz <code@raboof.com>
2026-05-14 15:10:20 +0200

    Make generated module interfaces public in simple sample set

--- samples/simple/ExamplePythonDependency/Public.cs
+++ samples/simple/ExamplePythonDependency/Public.cs
@@ -1,7 +1,16 @@
 namespace CSnakes.Runtime;
 
 public partial class HelloWorldExtensions;
+public partial interface IHelloWorld;
+
 public partial class KmeansExampleExtensions;
+public partial interface IKmeansExample;
+
 public partial class Phi3DemoExtensions;
+public partial interface IPhi3Demo;
+
 public partial class QuickDemoExtensions;
+public partial interface IQuickDemo;
+
 public partial class TypeDemosExtensions;
+public partial interface ITypeDemos;
